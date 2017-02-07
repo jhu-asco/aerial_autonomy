65d4f0f0220c16bf97356123c60a8ec57cad26a7
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2017-02-07 05:52:32 -0500

    Add GUI to trigger events, receive status
    Add rqt plugin to take in an event file and generate GUI with
    buttons corresponding to event names. The GUI also contains three
    textboxes to receive string data for status of Quad, arm, state machine
    The arm textbox is usually hidden and can be shown using --use_arm option

--- tests/event_manager_tests/event_manager_tests.cpp
+++ tests/event_manager_tests/event_manager_tests.cpp
@@ -8,43 +8,42 @@ using namespace std;
 
 //// \brief Definitions
 struct LSM {
-	template<class Event>
-	void process_event(Event &evt) {
-		std::cout<<"Event type : "<<typeid(evt).name()<<std::endl;
-	}
+  template <class Event> void process_event(Event &evt) {
+    std::cout << "Event type : " << typeid(evt).name() << std::endl;
+  }
 };
 ////
 
 /// \brief TEST
 /// All the tests are defined here
 namespace basic_events{
-	TEST(BasicEventManagerTest, InstantiateManager) {
-		ASSERT_NO_THROW(new BasicEventManager<LSM>());
-	}
-	TEST(BasicEventManagerTest, PrintEventMap) {
-		BasicEventManager<LSM> evt_manager;
-		ASSERT_NO_THROW(evt_manager.printEventList());
-	}
-	TEST(BasicEventManagerTest, TriggerEvents) {
-		BasicEventManager<LSM> evt_manager;
-    LSM lsm;
-	  evt_manager.triggerEvent("Land", lsm);
-	  evt_manager.triggerEvent("Takeoff", lsm);
-	  evt_manager.triggerEvent("Abort", lsm);
-	}
+TEST(BasicEventManagerTest, InstantiateManager) {
+  ASSERT_NO_THROW(new BasicEventManager<LSM>());
+}
+TEST(BasicEventManagerTest, PrintEventMap) {
+  BasicEventManager<LSM> evt_manager;
+  ASSERT_NO_THROW(evt_manager.printEventList());
+}
+TEST(BasicEventManagerTest, TriggerEvents) {
+  BasicEventManager<LSM> evt_manager;
+  LSM lsm;
+  evt_manager.triggerEvent("Land", lsm);
+  evt_manager.triggerEvent("Takeoff", lsm);
+  evt_manager.triggerEvent("Abort", lsm);
+}
 }
 namespace visual_servoing_events{
-	TEST(VisualServoingEventManagerTest, InstantiateManager) {
-		ASSERT_NO_THROW(new VisualServoingEventManager<LSM>());
-	}
-	TEST(VisualServoingEventManagerTest, TriggerEvents) {
-		VisualServoingEventManager<LSM> evt_manager;
-    LSM lsm;
-	  evt_manager.triggerEvent("Land", lsm);
-	  evt_manager.triggerEvent("Takeoff", lsm);
-	  evt_manager.triggerEvent("Abort", lsm);
-	  evt_manager.triggerEvent("FollowTrajectory", lsm);
-	}
+TEST(VisualServoingEventManagerTest, InstantiateManager) {
+  ASSERT_NO_THROW(new VisualServoingEventManager<LSM>());
+}
+TEST(VisualServoingEventManagerTest, TriggerEvents) {
+  VisualServoingEventManager<LSM> evt_manager;
+  LSM lsm;
+  evt_manager.triggerEvent("Land", lsm);
+  evt_manager.triggerEvent("Takeoff", lsm);
+  evt_manager.triggerEvent("Abort", lsm);
+  evt_manager.triggerEvent("FollowTrajectory", lsm);
+}
 }
 ///
 
