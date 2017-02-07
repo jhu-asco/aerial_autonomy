ce4a85c4de42a7e8d52a570772cec5d9e8121fe5
Gowtham Garimella <ggarime1@jhu.edu>
2017-02-06 23:05:45 -0500

    Modified variable names, fixed clang format

--- include/aerial_autonomy/logic_states/base_functors.h
+++ include/aerial_autonomy/logic_states/base_functors.h
@@ -3,48 +3,61 @@
  * Derived states have to implement the specific run function
  * behavior using this base class
  */
-template <class RobotSystemT, class LSMT> struct BaseRunFunctor {
+template <class RobotSystemT, class LogicStateMachineT> struct BaseRunFunctor {
   /**
    * @brief Override this run function for different sub classes.
    * This function performs the logic checking for each state
-   * @param rs Provides sensor data and allows for controlling hardware
-   * @param lsm Backend of logic State Machine. can send events using this.
+   * @param robot_system Provides sensor data and allows for controlling
+   * hardware
+   * @param logic_state_machine Backend of logic State Machine. can send events
+   * using this.
    */
-  virtual void run(RobotSystemT const &rs, LSMT &lsm) = 0;
+  virtual void run(RobotSystemT const &robot_system,
+                   LogicStateMachineT &logic_state_machine) = 0;
 
   /**
      * @brief operator () Internally calls run function
-     * @param rs Provides sensor data and allows for controlling hardware
-     * @param lsm Backend of logic State Machine. can send events using this.
+     * @param robot_system Provides sensor data and allows for controlling
+     * hardware
+     * @param logic_state_machine Backend of logic State Machine. can send
+     * events using this.
      */
   template <class SourceState, class TargetState>
-  void operator()(RobotSystemT const &rs, LSMT &lsm, SourceState &,
+  void operator()(RobotSystemT const &robot_system,
+                  LogicStateMachineT &logic_state_machine, SourceState &,
                   TargetState &) {
-    run(rs, lsm);
+    run(robot_system, logic_state_machine);
   }
 
   virtual ~BaseRunFunctor() {}
 };
 
-template <class RobotSystemT, class LSMT> struct BaseGuardFunctor {
+template <class RobotSystemT, class LogicStateMachineT>
+struct BaseGuardFunctor {
   /**
    * @brief Override this guard function for different sub classes.
    * This function decides whether to call the run function for each state
-   * @param rs Provides sensor data and allows for controlling hardware
-   * @param lsm Backend of logic State Machine. can send events using this.
+   * @param robot_system Provides sensor data and allows for controlling
+   * hardware
+   * @param logic_state_machine Backend of logic State Machine. can send events
+   * using this.
    */
-  virtual bool guard(RobotSystemT const &rs, LSMT &lsm) = 0;
+  virtual bool guard(RobotSystemT const &robot_system,
+                     LogicStateMachineT &logic_state_machine) = 0;
 
   /**
    * @brief operator () Internally calls guard function
-   * @param rs Provides sensor data and allows for controlling hardware
-   * @param lsm Backend of logic State Machine. can send events using this.
+   * @param robot_system Provides sensor data and allows for controlling
+   * hardware
+   * @param logic_state_machine Backend of logic State Machine. can send events
+   * using this.
    * @return the result of checking guard for the state
    */
   template <class SourceState, class TargetState>
-  bool operator()(RobotSystemT const &rs, LSMT &lsm, SourceState &,
+  bool operator()(RobotSystemT const &robot_system,
+                  LogicStateMachineT &logic_state_machine, SourceState &,
                   TargetState &) {
-    return guard(rs, lsm);
+    return guard(robot_system, logic_state_machine);
   }
   /**
      * @brief Destructor
