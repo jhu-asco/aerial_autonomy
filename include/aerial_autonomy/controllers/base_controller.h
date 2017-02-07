fd7dea2417aee32b9764eb18c40cde65849a8aaa
Gowtham Garimella <ggarime1@jhu.edu>
2017-02-06 21:31:05 -0500

    Add comments, and use pragma instead of define

--- include/aerial_autonomy/controllers/base_controller.h
+++ include/aerial_autonomy/controllers/base_controller.h
@@ -1,17 +1,14 @@
-#ifndef BASE_CONTROLLER_H
-#define BASE_CONTROLLER_H
-
+#pragma once
 template <class SensorDataType, class GoalType, class ControlType>
 class Controller {
 public:
   /**
    * @brief Run the control loop and return control arguments
    * @param sensor_data Data required for control loop. Can also be
-   * estimatordata
+   * estimator data
    * @return Control values to send to hardware
    */
   virtual ControlType run(SensorDataType sensor_data) = 0;
-  // lock and set the desired goal for the control logic
   /**
    * @brief set the goal condition for the controller. Should use
    * internal locking as the run function can be called from a separate thread
@@ -23,4 +20,3 @@ public:
    */
   virtual ~Controller() {}
 };
-#endif // BASE_CONTROLLER_H
