9820321cb6de96e12c61cf51c0b2d5e404ec9b0d
Matthew Sheckells <msheckells@gmail.com>
2017-02-08 15:20:29 -0500

    Add pluginlib and class_loader dependencies.  Tests now compile and pass.

--- include/aerial_autonomy/types/position.h
+++ include/aerial_autonomy/types/position.h
@@ -1,5 +1,7 @@
 #pragma once
 struct Position {
+  Position() : x(0), y(0), z(0) {}
+  Position(double x, double y, double z) : x(x), y(y), z(z) {}
   double x;
   double y;
   double z;
--- tests/controller_hardware_connectors/position_controller_drone_connector_tests.cpp
+++ tests/controller_hardware_connectors/position_controller_drone_connector_tests.cpp
@@ -4,15 +4,34 @@
 #include <parsernode/parser.h>
 #include <gtest/gtest.h>
 
-/// \brief TEST
-/// All the tests are defined here
+/// \brief Test BuiltInPositionController
 TEST(PositionControllerDroneConnectorTests, Constructor) {
   pluginlib::ClassLoader<parsernode::Parser>* parser_loader = 
     new pluginlib::ClassLoader<parsernode::Parser>("parsernode","parsernode::Parser");
+
   parsernode::Parser* drone_hardware = 
     parser_loader->createInstance("quad_simulator_parser/QuadSimParser").get();
+
   BuiltInPositionController position_controller;
+
   ASSERT_NO_THROW(new PositionControllerDroneConnector(*drone_hardware, position_controller));
+}
+
+TEST(PositionControllerDroneConnectorTests, SetGoal) {
+  pluginlib::ClassLoader<parsernode::Parser> *parser_loader =
+      new pluginlib::ClassLoader<parsernode::Parser>("parsernode",
+                                                     "parsernode::Parser");
+
+  parsernode::Parser *drone_hardware =
+      parser_loader->createInstance("quad_simulator_parser/QuadSimParser")
+          .get();
+
+  BuiltInPositionController position_controller;
+
+  auto pos_controller_connector = new PositionControllerDroneConnector(
+      *drone_hardware, position_controller);
+
+  pos_controller_connector->setGoal(PositionYaw(10, 10, 10, 0.1));
 }
 ///
 
