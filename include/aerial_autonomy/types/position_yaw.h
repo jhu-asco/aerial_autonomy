4f2550c2ebf3268f68fc59834ba6078c0aab14b2
Matthew Sheckells <msheckells@gmail.com>
2017-02-08 15:20:29 -0500

    Add pluginlib and class_loader dependencies.  Tests now compile and pass.

--- include/aerial_autonomy/types/position_yaw.h
+++ include/aerial_autonomy/types/position_yaw.h
@@ -2,5 +2,8 @@
 #include "aerial_autonomy/types/position.h"
 
 struct PositionYaw : public Position {
+  PositionYaw() : Position(), yaw(0) {}
+  PositionYaw(double x, double y, double z, double yaw)
+      : Position(x, y, z), yaw(yaw) {}
   double yaw;
 };
