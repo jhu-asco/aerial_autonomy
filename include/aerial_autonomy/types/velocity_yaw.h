9341e09e1125e9426bd81643b6d9a5b6b0f63dfc
Matthew Sheckells <msheckells@gmail.com>
2017-02-12 16:29:34 -0500

    Add new more constructors for PositionYaw and VelocityYaw

--- include/aerial_autonomy/types/velocity_yaw.h
+++ include/aerial_autonomy/types/velocity_yaw.h
@@ -3,6 +3,7 @@
 
 struct VelocityYaw : public Velocity {
   VelocityYaw() : Velocity(), yaw(0) {}
+  VelocityYaw(Velocity v, double yaw) : Velocity(v), yaw(yaw) {}
   VelocityYaw(double x, double y, double z, double yaw)
       : Velocity(x, y, z), yaw(yaw) {}
   double yaw;
