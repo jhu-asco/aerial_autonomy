34c226ee47e38b4699ea4b3833b85603006a8d3c
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2017-02-22 15:07:27 +0530

    Create generic action, guard functors - transition
    These generic action, guard functors will be used to create
    transition actions, guards along with internal actions. The
    actions use the robot system from logic state machine directly.

--- tests/logic_states/base_state_tests.cpp
+++ tests/logic_states/base_state_tests.cpp
@@ -1,48 +1,66 @@
-#include <aerial_autonomy/logic_states/base_functors.h>
+#include <aerial_autonomy/actions_guards/base_functors.h>
 #include <aerial_autonomy/logic_states/base_state.h>
 #include <gtest/gtest.h>
 
 //// \brief Definitions
 ///  Define any necessary subclasses for tests here
-struct RobotSystem {};
-struct LogicStateMachine {};
+struct RobotSystem {
+  void setGoal(){};
+};
+struct LogicStateMachine {
+  LogicStateMachine(RobotSystem &robot_system) : robot_system_(robot_system) {}
+  // Add friend classes:
+  template <class EventT, class RobotSystemT, class LogicStateMachineT>
+  friend class ActionFunctor;
 
-struct EmptyRunFunctor : BaseRunFunctor<RobotSystem, LogicStateMachine> {
-  virtual void run(RobotSystem const &, LogicStateMachine &) {}
+  template <class EventT, class RobotSystemT, class LogicStateMachineT>
+  friend class GuardFunctor;
+
+protected:
+  RobotSystem &robot_system_;
 };
 
-struct EmptyGuardFunctor : BaseGuardFunctor<RobotSystem, LogicStateMachine> {
-  virtual bool guard(RobotSystem const &, LogicStateMachine &) { return true; }
+struct EmptyActionFunctor
+    : InternalActionFunctor<RobotSystem, LogicStateMachine> {
+  virtual void run(InternalTransitionEvent const &, RobotSystem &,
+                   LogicStateMachine &) {}
+};
+
+struct EmptyGuardFunctor
+    : GuardFunctor<double, RobotSystem, LogicStateMachine> {
+  virtual bool guard(double const &, RobotSystem &, LogicStateMachine &) {
+    return true;
+  }
 };
 ////
 
 /// \brief TEST
 /// All the tests are defined here
 TEST(BaseStateTests, BaseStateCtor) {
-  typedef BaseState<RobotSystem, LogicStateMachine, EmptyRunFunctor>
+  typedef BaseState<RobotSystem, LogicStateMachine, EmptyActionFunctor>
       BaseStateEmpty;
   ASSERT_NO_THROW(BaseStateEmpty());
-
-  typedef BaseState<RobotSystem, LogicStateMachine, EmptyRunFunctor,
-                    EmptyGuardFunctor>
-      BaseStateEmptyGuard;
-  ASSERT_NO_THROW(BaseStateEmptyGuard());
 }
 
 TEST(BaseStateTests, EmptytFctor) {
-  EmptyRunFunctor empty_functor;
+  EmptyActionFunctor empty_functor;
   RobotSystem robot_system;
-  LogicStateMachine logic_state_machine;
+  LogicStateMachine logic_state_machine(robot_system);
   // Robotsystem, LogicStateMachine, source, target
-  ASSERT_NO_THROW(empty_functor.run(robot_system, logic_state_machine));
+  ASSERT_NO_THROW(empty_functor.run(InternalTransitionEvent(), robot_system,
+                                    logic_state_machine));
+  int empty_source_state, empty_target_state;
+  ASSERT_NO_THROW(empty_functor(InternalTransitionEvent(), logic_state_machine,
+                                empty_source_state, empty_target_state));
 }
 
 TEST(BaseStateTests, EmptytGuardFctor) {
   EmptyGuardFunctor empty_functor;
   RobotSystem robot_system;
-  LogicStateMachine logic_state_machine;
+  LogicStateMachine logic_state_machine(robot_system);
   // Robotsystem, LogicStateMachine, source, target
-  ASSERT_TRUE(empty_functor.guard(robot_system, logic_state_machine));
+  ASSERT_TRUE(
+      empty_functor.guard(double(0.0), robot_system, logic_state_machine));
 }
 ///
 
