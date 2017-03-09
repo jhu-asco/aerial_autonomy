13f93efffe0f22b9343ba937c0c0d1da3846f8c2
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2017-03-09 23:48:13 +0530

    add test for constant acceleration

--- tests/controller_hardware_connectors/manual_rpyt_controller_drone_connector_tests.cpp
+++ tests/controller_hardware_connectors/manual_rpyt_controller_drone_connector_tests.cpp
@@ -72,9 +72,46 @@ TEST(ManualRPYTControllerDroneConnectorTests, Run) {
   ASSERT_NEAR(sensor_data.rpydata.y, 0.00261799, 1e-4);
   // Verify Omegaz
   ASSERT_NEAR(sensor_data.omega.z, -0.00314, 1e-3);
+}
 
-  //\todo (matt) : Need to test thrust.  Could test if we have a parser with
-  // dynamics
+TEST(ManualRPYTControllerDroneConnectorTests, RunConstantAcceleration) {
+  QuadSimulator drone_hardware;
+  drone_hardware.takeoff();
+  drone_hardware.set_delay_send_time(0.02);
+
+  // Create controller and connector
+  ManualRPYTController manual_rpyt_controller;
+  ManualRPYTControllerDroneConnector manual_rpyt_controller_connector(
+      drone_hardware, manual_rpyt_controller);
+
+  // Test set goal
+  manual_rpyt_controller_connector.setGoal(EmptyGoal());
+
+  // Set stick commands
+  int16_t channels[4] = {0, 0, 2000, 0};
+  drone_hardware.setRC(channels);
+
+  parsernode::common::quaddata first_data, second_data, third_data;
+  // Test run
+  drone_hardware.getquaddata(first_data);
+  for (int i = 0; i < 50; ++i) {
+    manual_rpyt_controller_connector.run();
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+  }
+  drone_hardware.getquaddata(second_data);
+  ASSERT_NEAR(second_data.localpos.x, first_data.localpos.x, 1e-4);
+  ASSERT_NEAR(second_data.localpos.y, first_data.localpos.y, 1e-4);
+  // for 1 more second
+  for (int i = 0; i < 50; ++i) {
+    manual_rpyt_controller_connector.run();
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+  }
+  drone_hardware.getquaddata(third_data);
+  ASSERT_NEAR(third_data.localpos.x, first_data.localpos.x, 1e-4);
+  ASSERT_NEAR(third_data.localpos.y, first_data.localpos.y, 1e-4);
+  double zdiff_third = third_data.localpos.z - first_data.localpos.z;
+  double zdiff_second = second_data.localpos.z - first_data.localpos.z;
+  ASSERT_NEAR(zdiff_third, 4 * zdiff_second, 5e-2);
 }
 ///
 
