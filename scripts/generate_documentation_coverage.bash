312a34647749497498561c9b7addfac4adaaed8e
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2017-03-11 15:50:05 +0530

    Fix documentation coverage script check lcov errors
    If there is keyword error in lcov output, then fails pushing
    The solution then is to remove the build folder and rebuild.
    This is a known problem with lcov code generation script
    when the source files change.

--- scripts/generate_documentation_coverage.bash
+++ scripts/generate_documentation_coverage.bash
@@ -4,6 +4,8 @@ cd `git rev-parse --show-toplevel`
 echo $PWD
 echo "Generating Docs"
 doxygen > /dev/null # Generates docs
+# Create coverage folder if it does not exist
+[ -d documentation_coverage_info ] || mkdir documentation_coverage_info
 echo "Coverage report"
 python -m coverxygen --xml-dir ./docs/xml --src-dir . --output ./documentation_coverage_info/doc_coverage.info > /dev/null
 echo "Generate HTML Report"
