151a7c13efb7c745cbfebafa600ac26a2ffd1a0a
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2017-03-14 09:36:44 +0530

    Exclude bash scripts from grep
    Also revert to original state if failed

--- scripts/applydocs.bash
+++ scripts/applydocs.bash
@@ -4,6 +4,8 @@ if ( ! git diff-index --quiet HEAD -- ); then
   echo "ERROR: uncommited changes exist"
   exit 1
 else
+  # Go to root of git
+  cd `git rev-parse --show-toplevel`
   doxygen
   git config merge.renameLimit 999999
   echo "Adding docs to git"
@@ -14,7 +16,17 @@ else
   git checkout gh-pages > /dev/null
   echo "Applying docs to gh-pages"
   git stash pop > /dev/null 2> /dev/null
-  grep -lr '<<<<<<< ' . | xargs git checkout --theirs
+  set -o pipefail
+  grep --exclude=\*.{bash,swp} -lr '<<<<<<< ' . | xargs git checkout --theirs
+  error=$?
+  if [ $error -ne 0 ]; then
+    echo "Failed to checkout applydocs"
+    echo "Reverting to original state"
+    git reset HEAD
+    git checkout .
+    git checkout $branch_name
+    exit 1
+  fi
   git add -u . 
   git config --unset merge.renameLimit
   echo "Commiting docs"
