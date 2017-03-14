dc507f0e2fb115161909662a5f569790cea630d4
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2017-03-14 09:09:37 +0530

    Improve applydocs script

--- scripts/applydocs.bash
+++ scripts/applydocs.bash
@@ -1,9 +1,24 @@
-doxygen
-git config merge.renameLimit 999999
-git add -f docs
-git stash 
-git checkout gh-pages
-git stash pop
-grep -lr '<<<<<<< ' . | xargs git checkout --theirs
-git add -u .
-git config --unset merge.renameLimit
+#!/bin/bash
+branch_name=`git rev-parse --abbrev-ref HEAD`
+if ( ! git diff-index --quiet HEAD -- ); then
+  echo "ERROR: uncommited changes exist"
+  exit 1
+else
+  doxygen
+  git config merge.renameLimit 999999
+  echo "Adding docs to git"
+  git add -f docs > /dev/null
+  echo "Stashing docs"
+  git stash  > /dev/null
+  echo "Checking gh-pages"
+  git checkout gh-pages > /dev/null
+  echo "Applying docs to gh-pages"
+  git stash pop > /dev/null 2> /dev/null
+  grep -lr '<<<<<<< ' . | xargs git checkout --theirs
+  git add -u . 
+  git config --unset merge.renameLimit
+  echo "Commiting docs"
+  git commit -m "Applying Docs from $branch_name on $( date )"
+  echo "Reverting to original branch"
+  git checkout $branch_name
+fi
