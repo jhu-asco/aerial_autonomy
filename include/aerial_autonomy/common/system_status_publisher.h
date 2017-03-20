96f4cc8f6922461f712585e320e771edc9b01fe1
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2017-03-20 15:18:48 -0400

    Create common system handler to minimize code dupl
    Create "CommonSystemHandlerInterface" class which is used
    as a member variable "UAVSystemHandler". The common system handler
    instantiates state machine by taking robot system as reference.
    It also creates timers for publishing system status and
    running state machine internal event processing loop.
    
    The proto config files are also separated into UAVhandler config
    and common system handler config. Indvidual system handlers
    should include appropriate common system handler config to
    pass to CommonSystemHandlerInterface.
    
    Thets are also modified to account for new names.

--- include/aerial_autonomy/common/system_status_publisher.h
+++ include/aerial_autonomy/common/system_status_publisher.h
@@ -2,25 +2,25 @@
 
 #include <ros/ros.h>
 
-#include <aerial_autonomy/robot_systems/uav_system.h>
-
 /**
  * @brief Responsible for publishing system status message
  * @tparam Logic state machine type that we are getting status from
  */
-template <class LogicStateMachineT> class SystemStatusPublisher {
+template <class LogicStateMachineT, class RobotSystemT>
+class SystemStatusPublisher {
 public:
   /**
    * @brief Constructor
    * @param nh NodeHandle to use for publishing system status
-   * @param uav_system UAVSystem whose status will be published
+   * @param robot_system RobotSystem whose status will be published
    * @param logic_state_machine State machine whose status will be published
    */
-  SystemStatusPublisher(ros::NodeHandle &nh, UAVSystem &uav_system,
+  SystemStatusPublisher(ros::NodeHandle &nh, RobotSystemT &robot_system,
                         LogicStateMachineT &logic_state_machine)
       : nh_(nh),
         system_status_pub_(nh.advertise<std_msgs::String>("system_status", 1)),
-        uav_system_(uav_system), logic_state_machine_(logic_state_machine) {}
+        robot_system_(robot_system), logic_state_machine_(logic_state_machine) {
+  }
 
   /**
   * @brief Publish system status and state machine status
@@ -28,12 +28,12 @@ public:
   * to automatically add table lines
   */
   void publishSystemStatus() {
-    std::string uav_system_status = uav_system_.getSystemStatus();
+    std::string robot_system_status = robot_system_.getSystemStatus();
     std::string current_state_name = pstate(logic_state_machine_);
     std::string no_transition_event_name =
         logic_state_machine_.get_no_transition_event_index().name();
     std_msgs::String status;
-    status.data = "UAV System Status:\n" + uav_system_status + "\n";
+    status.data = "Robot System Status:\n" + robot_system_status + "\n";
     status.data += "\n\n========================\n\n";
     status.data += "Logic State Machine Status: \n";
     status.data += "Current state:\t" + current_state_name + "\n";
@@ -45,7 +45,7 @@ public:
 private:
   ros::NodeHandle &nh_;              ///< NodeHandle used for publishing
   ros::Publisher system_status_pub_; ///< publishes status messages
-  UAVSystem &uav_system_;            ///< system whose status we are publishing
+  RobotSystemT &robot_system_;       ///< system whose status we are publishing
   LogicStateMachineT
       &logic_state_machine_; ///< state machine whose status we are publishing
 };
---
+++ include/aerial_autonomy/system_handlers/uav_system_handler.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <ros/ros.h>
+
+#include <parsernode/parser.h>
+#include <pluginlib/class_loader.h>
+
+#include <aerial_autonomy/actions_guards/base_functors.h>
+#include <aerial_autonomy/robot_systems/uav_system.h>
+#include <aerial_autonomy/system_handlers/common_system_handler_interface.h>
+
+#include "uav_system_handler_config.pb.h"
+
+/**
+ * @brief Owns all of the autonomous system components and is responsible for
+ * thread management. Also owns a common system handler object to
+ * handle creation of state machine and connecting it to GUI.
+ *
+ * @tparam LogicStateMachineT Logic state machine to use
+ * @tparam EventManagerT Event manager to use
+ */
+template <class LogicStateMachineT, class EventManagerT>
+class UAVSystemHandler {
+public:
+  /**
+   * @brief Constructor
+   * @param nh NodeHandle to use for event and command subscription
+   * @param config Proto configuration parameters
+   */
+  UAVSystemHandler(ros::NodeHandle &nh, UAVSystemHandlerConfig &config)
+      : parser_loader_("parsernode", "parsernode::Parser"),
+        uav_hardware_(
+            parser_loader_.createUnmanagedInstance(config.uav_parser_type())),
+        uav_system_(*uav_hardware_, config.uav_system_config()),
+        common_handler_interface_(nh, config.base_config(), uav_system_),
+        uav_controller_timer_(
+            std::bind(&UAVSystem::runActiveController, std::ref(uav_system_),
+                      HardwareType::UAV),
+            std::chrono::milliseconds(config.uav_controller_timer_duration())) {
+    // Initialize UAV plugin
+    uav_hardware_->initialize(nh);
+
+    // Get the party started
+    common_handler_interface_.startTimers();
+    uav_controller_timer_.start();
+  }
+
+  /**
+  * @brief Delete copy constructor
+  */
+  UAVSystemHandler(const UAVSystemHandler &) = delete;
+
+  /**
+   * @brief Get UAV state
+   * @return The UAV state
+   */
+  parsernode::common::quaddata getUAVData() {
+    parsernode::common::quaddata quad_data;
+    uav_hardware_->getquaddata(quad_data);
+    return quad_data;
+  }
+
+  /**
+  * @brief Forward common handler connected function for testing
+  * is GUI is connected to this node or not
+  *
+  * @return true if connected
+  */
+  bool isConnected() { return common_handler_interface_.isConnected(); }
+
+private:
+  pluginlib::ClassLoader<parsernode::Parser>
+      parser_loader_; ///< Used to load hardware plugin
+  std::unique_ptr<parsernode::Parser> uav_hardware_; ///< Hardware instance
+  UAVSystem uav_system_;                             ///< Contains controllers
+  CommonSystemHandlerInterface<LogicStateMachineT, EventManagerT, UAVSystem>
+      common_handler_interface_;    ///< Common logic to create state machine
+                                    ///< and associated connections.
+  AsyncTimer uav_controller_timer_; ///< Timer for running uav controller
+};
