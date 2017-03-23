29a3433ee663d03ae8a17562b034f05aac143d87
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2017-03-23 14:32:58 -0400

    Add arm system and tests

---
+++ tests/robot_systems/arm_system_tests.cpp
@@ -0,0 +1,17 @@
+#include <aerial_autonomy/robot_systems/arm_system.h>
+#include <chrono>
+#include <gtest/gtest.h>
+#include <thread>
+
+/// \brief Test Arm System
+TEST(ArmSystemTests, Constructor) {
+  ros::NodeHandle nh;
+  ASSERT_NO_THROW(new ArmSystem(nh));
+}
+///
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "arm_system_tests");
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
