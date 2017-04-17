a617c370798fb7e1c57b7556abca4a1764a0c564
Matthew Sheckells <msheckells@gmail.com>
2017-04-17 16:44:10 -0400

    Remove build folder before test coverae generation. Make test/documentation coverage info folders hidden to reduce clutter

--- scripts/generate_documentation_coverage.bash
+++ scripts/generate_documentation_coverage.bash
@@ -5,10 +5,10 @@ echo $PWD
 echo "Generating Docs"
 doxygen > /dev/null # Generates docs
 # Create coverage folder if it does not exist
-[ -d documentation_coverage_info ] || mkdir documentation_coverage_info
+[ -d .documentation_coverage_info ] || mkdir .documentation_coverage_info
 echo "Coverage report"
-python -m coverxygen --xml-dir ./docs/xml --src-dir . --output ./documentation_coverage_info/doc_coverage.info > /dev/null
+python -m coverxygen --xml-dir ./docs/xml --src-dir . --output ./.documentation_coverage_info/doc_coverage.info > /dev/null
 echo "Generate HTML Report"
-genhtml --no-function-coverage --no-branch-coverage documentation_coverage_info/doc_coverage.info -o ./documentation_coverage_info > /dev/null
+genhtml --no-function-coverage --no-branch-coverage .documentation_coverage_info/doc_coverage.info -o ./.documentation_coverage_info > /dev/null
 echo "Print Summary"
-lcov --summary ./documentation_coverage_info/doc_coverage.info
+lcov --summary ./.documentation_coverage_info/doc_coverage.info
