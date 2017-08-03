5ff70deb950f5bb663b8b171771590dd6985a6ac
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2017-08-03 10:36:50 -0400

    Add documentation for helper file

--- include/aerial_autonomy/common/html_table_writer.h
+++ include/aerial_autonomy/common/html_table_writer.h
@@ -4,13 +4,31 @@
 #include <stdexcept>
 #include <string>
 
+/**
+ * @brief Helper class to write html tables to text format
+ */
 class HtmlTableWriter {
 public:
+  /**
+   * @brief constructor initializes the table header
+   */
   HtmlTableWriter() : table_ended(false), row_ended(true) {
     table_string_stream << std::fixed << std::setprecision(2);
     table_string_stream << "<table border=\"1\" width=\"100\%\">";
   }
 
+  /**
+   * @brief Add a cell to the table. Should be called after
+   * beginning a row. Otherwise will throw an exception
+   *
+   * @tparam DataT Type of data to add to cell
+   * @param data value of the data to add
+   * @param header If header is provided it is added along with data to same
+   * cell
+   * @param bg_color Background color of the cell to use (Default white) The
+   * color
+   *        is provided as a hexcode #RGB where R, G, B go from 0 to F
+   */
   template <typename DataT>
   void addCell(const DataT &data, std::string header = "",
                std::string bg_color = "#FFF") {
@@ -25,6 +43,9 @@ public:
     table_string_stream << "</td>";
   }
 
+  /**
+   * @brief Begin a new row in the table
+   */
   void beginRow() {
     if (!row_ended) {
       endRow();
@@ -33,6 +54,14 @@ public:
     row_ended = false;
   }
 
+  /**
+   * @brief Add a table header. Should be called after beginning a row
+   *
+   * This is similar to a addCell but is bold and is placed at the top of the
+   * table
+   *
+   * @param header Header value to add.
+   */
   void addHeader(std::string header) {
     if (row_ended) {
       throw std::runtime_error("Cannot add cell without beginning row");
@@ -42,6 +71,11 @@ public:
     table_string_stream << "</th>";
   }
 
+  /**
+   * @brief Get the html table in string format
+   *
+   * @return string of html table
+   */
   std::string getTableString() {
     if (!row_ended) {
       endRow();
@@ -54,8 +88,12 @@ public:
 
 private:
   std::stringstream table_string_stream; ///< Html table in string format
-  bool table_ended;
-  bool row_ended;
+  bool table_ended; ///< Flag to check if table has been completed
+  bool row_ended;   ///< Flag to check if row has ended
+
+  /**
+   * @brief Add the table footer
+   */
   void endTable() {
     if (!row_ended) {
       endRow();
@@ -63,6 +101,9 @@ private:
     table_ended = true;
     table_string_stream << "</table>";
   }
+  /**
+   * @brief Add row footer
+   */
   void endRow() {
     row_ended = true;
     table_string_stream << "</tr>";
