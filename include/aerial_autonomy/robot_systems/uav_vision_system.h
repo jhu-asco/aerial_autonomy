5faab90de951ead104e79d060ed94098eee3bc27
Gowtham Garimella <ggarime1@jhu.edu>
2017-08-11 16:11:50 +0000

    Publish tracking vector in Quad frame, update cam transform

--- include/aerial_autonomy/robot_systems/uav_vision_system.h
+++ include/aerial_autonomy/robot_systems/uav_vision_system.h
@@ -64,11 +64,12 @@ public:
     std::unordered_map<uint32_t, Position> tracking_vectors;
     if (tracker_.getTrackingVectors(tracking_vectors)) {
       for (auto tv : tracking_vectors) {
+        tf::Vector3 tv_body_frame = camera_transform_*tf::Vector3(tv.second.x, tv.second.y, tv.second.z);
         table_writer.beginRow();
         table_writer.addCell(tv.first);
-        table_writer.addCell(tv.second.x);
-        table_writer.addCell(tv.second.y);
-        table_writer.addCell(tv.second.z);
+        table_writer.addCell(tv_body_frame.x());
+        table_writer.addCell(tv_body_frame.y());
+        table_writer.addCell(tv_body_frame.z());
       }
     }
     status << table_writer.getTableString();
