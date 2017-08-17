0ffdd6aa4a57393d20e37212333f197c569a5f3d
soham <spatkar1@jhu.edu>
2017-08-17 18:27:51 -0400

    Added manual velocity controller

---
+++ include/aerial_autonomy/controllers/manual_velocity_controller.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "aerial_autonomy/controllers/base_controller.h"
+#include "aerial_autonomy/types/empty_goal.h"
+#include "aerial_autonomy/types/joysticks_yaw.h"
+#include "aerial_autonomy/types/velocity_yaw.h"
+#include "aerial_autonomy/types/roll_pitch_yaw_thrust.h"
+#include "aerial_autonomy/controllers/rpyt_based_velocity_controller.h"
+
+/**
+ * @brief A controller that passes joystick commands to a drone's RPYT
+ * controller
+ */
+class ManualVelocityController
+: public Controller<std::tuple<JoysticksYaw, VelocityYaw>, EmptyGoal, RollPitchYawThrust> {
+public:
+  /**
+   * @brief Destructor
+   */
+  virtual ~ManualVelocityController() {}
+
+protected:
+  /**
+   * @brief Run the control loop.  Converts Joystick commands to Velocity.
+   * @param sensor_data Joystick commands to be converted into Velocity.
+   * @param goal Goal is not used here
+   * @param control Velocity to send to hardware
+   * return True if successfully converted sensor data to control
+   */
+  virtual bool runImplementation(std::tuple<JoysticksYaw, VelocityYaw> sensor_data,
+    EmptyGoal goal,
+    RollPitchYawThrust &control);
+  /**
+  * @brief Default implementation since there is no concept of convergence
+  * for manual rpyt controller
+  * @return True always
+  */
+  virtual bool isConvergedImplementation(std::tuple<JoysticksYaw, VelocityYaw> sensor_data, EmptyGoal goal) {
+    return true;
+  }
+
+private:
+    /**
+  * @brief Generic map function to map input range to output range
+  *
+  * @param input Input value to map
+  * @param input_min Min for input
+  * @param input_max Max for input
+  * @param output_min Min for output
+  * @param output_max Max for output
+  *
+  * @return Map the input based on input range to output in output range
+  */
+  double map(double input, double input_min, double input_max,
+   double output_min, double output_max);
+  /**
+  * @brief Internal controller to get rpyt from desired velocity
+  */
+  RPYTBasedVelocityController rpyt_vel_ctlr;
+};
---
+++ src/controllers/manual_velocity_controller.cpp
@@ -0,0 +1,40 @@
+#include "aerial_autonomy/controllers/manual_velocity_controller.h"
+
+bool ManualVelocityController::runImplementation(std::tuple<JoysticksYaw, VelocityYaw> sensor_data,
+                                              EmptyGoal goal,
+                                              RollPitchYawThrust &control){
+
+  VelocityYaw vel_goal;
+  JoysticksYaw joy_sensor_data = std::get<0>(sensor_data);
+
+  vel_goal.x = map(joy_sensor_data.channel1, -10000, 10000, -1, 1);
+  vel_goal.y = map(joy_sensor_data.channel2, -10000, 10000, -1, 1);
+  vel_goal.z = map(joy_sensor_data.channel3, -10000, 10000, -1, 1);
+
+  double yaw = map(joy_sensor_data.channel4, -10000, 10000, -M_PI, M_PI);
+
+    vel_goal.y = joy_sensor_data.yaw - yaw * 0.02;
+  if (vel_goal.y > M_PI)
+    vel_goal.y = vel_goal.y - 2 * M_PI;
+  else if (vel_goal.y < -M_PI)
+    vel_goal.y = vel_goal.y + 2 * M_PI;
+
+  VelocityYaw vel_sensor_data = std::get<1>(sensor_data);
+
+  rpyt_vel_ctlr.setGoal(vel_goal);
+  rpyt_vel_ctlr.run(vel_sensor_data, control);
+  
+  return true;
+}
+
+double ManualVelocityController::map(double input, double input_min,
+                                 double input_max, double output_min,
+                                 double output_max) {
+  if (input > input_max)
+    return output_max;
+  else if (input < input_min)
+    return output_min;
+  return output_min +
+         ((input - input_min) * (output_max - output_min)) /
+             (input_max - input_min);
+}
