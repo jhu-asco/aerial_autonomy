028091550772323c691165ffdebe99bfb6e707f9
Matthew Sheckells <msheckells@gmail.com>
2017-08-21 17:36:10 -0400

    Add test for convergence within tolerance

--- tests/controllers/relative_pose_controller_tests.cpp
+++ tests/controllers/relative_pose_controller_tests.cpp
@@ -7,7 +7,7 @@ public:
   RelativePoseControllerTests() {
     auto tolerance = config_.mutable_goal_position_tolerance();
     tolerance->set_x(0.1);
-    tolerance->set_y(0.1);
+    tolerance->set_y(0.2);
     tolerance->set_z(0.1);
   }
 
@@ -68,6 +68,25 @@ TEST_F(RelativePoseControllerTests, ConvergedOffset) {
   RelativePoseController controller(config_);
   tf::Transform current_pose(tf::Quaternion(0, 0, 0, 1), tf::Vector3(9, 0, -1));
   tf::Transform tracked_pose(tf::Quaternion(0, 0, 0, 1), tf::Vector3(10, 0, 0));
+  tf::Transform goal(tf::Quaternion(), tf::Vector3(-1, 0, -1));
+  tf::Transform global_goal = tracked_pose * goal;
+  auto sensor_data = std::make_tuple(current_pose, tracked_pose);
+
+  controller.setGoal(goal);
+
+  tf::Transform controls;
+  bool result = controller.run(sensor_data, controls);
+
+  ASSERT_TRUE(result);
+  assert_tf_near(controls, global_goal);
+  ASSERT_TRUE(controller.isConverged(sensor_data));
+}
+
+TEST_F(RelativePoseControllerTests, ConvergedWithinTolerance) {
+  RelativePoseController controller(config_);
+  tf::Transform current_pose(tf::Quaternion(0, 0, 0, 1), tf::Vector3(9, 0, -1));
+  tf::Transform tracked_pose(tf::Quaternion(0, 0, 0, 1),
+                             tf::Vector3(10.09, .19, 0.09));
   tf::Transform goal(tf::Quaternion(), tf::Vector3(-1, 0, -1));
   tf::Transform global_goal = tracked_pose * goal;
   auto sensor_data = std::make_tuple(current_pose, tracked_pose);
