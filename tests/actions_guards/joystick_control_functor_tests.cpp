0fce528972068c2bf7ff54b89d6081f8f7a6aef9
soham <spatkar1@jhu.edu>
2017-09-08 17:23:26 -0400

    Add joystick control state machine

---
+++ tests/actions_guards/joystick_control_functor_tests.cpp
@@ -0,0 +1,145 @@
+#include <aerial_autonomy/actions_guards/joystick_control_states_actions.h>
+#include <aerial_autonomy/joystick_control_events.h>
+#include <aerial_autonomy/sensors/guidance.h>
+#include <aerial_autonomy/tests/sample_logic_state_machine.h>
+#include <gtest/gtest.h>
+#include <quad_simulator_parser/quad_simulator.h>
+
+/**
+* @brief Namespace for UAV Simulator
+*/
+using namespace quad_simulator;
+
+/**
+* @brief Namespace for Joystick states and actions
+* for UAVLogicStateMachine
+*/
+using jcsa = JoystickControlStatesActions<UAVLogicStateMachine>;
+
+/**
+* @brief Namesapce for joystick control events
+*/
+namespace jce = joystick_control_events;
+
+TEST(JoystickControlTests, Constructor) {
+  ASSERT_NO_THROW(new jcsa::JoystickControlAction());
+  ASSERT_NO_THROW(new jcsa::JoystickControlState());
+}
+
+TEST(JoystickControlTests, TransitionGuardValidTest) {
+
+  QuadSimulator drone_hardware;
+  std::cout << "creating uav_system\n";
+  UAVSystem uav_system(
+      drone_hardware, UAVSystemConfig(),
+      std::shared_ptr<Sensor<VelocityYaw>>(new Guidance(drone_hardware)));
+  UAVLogicStateMachine sample_logic_state_machine(uav_system);
+  jcsa::JoystickControlGuard transition_guard_functor;
+  int dummy_start_state, dummy_target_state;
+
+  bool result = transition_guard_functor(jce::JoystickControlEvent(),
+                                         sample_logic_state_machine,
+                                         dummy_start_state, dummy_target_state);
+
+  ASSERT_TRUE(result);
+}
+
+TEST(JoystickControlTests, TransitionActionTest) {
+  QuadSimulator drone_hardware;
+  UAVSystem uav_system(
+      drone_hardware, UAVSystemConfig(),
+      std::shared_ptr<Sensor<VelocityYaw>>(new Guidance(drone_hardware)));
+
+  UAVLogicStateMachine sample_logic_state_machine(uav_system);
+  jcsa::JoystickControlAction transition_action_functor;
+  int dummy_start_state, dummy_target_state;
+
+  transition_action_functor(jce::JoystickControlEvent(),
+                            sample_logic_state_machine, dummy_start_state,
+                            dummy_target_state);
+  uav_system.runActiveController(HardwareType::UAV);
+
+  ASSERT_EQ(uav_system.getStatus<JoystickVelocityControllerDroneConnector>(),
+            ControllerStatus::Active);
+}
+
+TEST(JoystickControlTests, ControllerRunTest) {
+  QuadSimulator drone_hardware;
+  UAVSystemConfig uav_system_config;
+  UAVSystem uav_system(
+      drone_hardware, uav_system_config,
+      std::shared_ptr<Sensor<VelocityYaw>>(new Guidance(drone_hardware)));
+
+  RPYTBasedVelocityControllerConfig rpyt_config_;
+  rpyt_config_.set_kp(2.0);
+  rpyt_config_.set_ki(0.01);
+
+  auto vel_ctlr_config = rpyt_config_.mutable_velocity_controller_config();
+  auto tolerance = vel_ctlr_config->mutable_goal_velocity_tolerance();
+  tolerance->set_vx(1e-4);
+  tolerance->set_vy(1e-4);
+  tolerance->set_vz(1e-4);
+
+  uav_system.updateRPYTVelocityControllerConfig(rpyt_config_);
+
+  UAVLogicStateMachine sample_logic_state_machine(uav_system);
+  jcsa::JoystickControlAction transition_action_functor;
+  int dummy_start_state, dummy_target_state;
+
+  transition_action_functor(jce::JoystickControlEvent(),
+                            sample_logic_state_machine, dummy_start_state,
+                            dummy_target_state);
+  // set rc channels
+  int16_t channels[4] = {1000, 1000, -1000, 1000};
+  drone_hardware.setRC(channels);
+  drone_hardware.set_delay_send_time(0.02);
+
+  uav_system.runActiveController(HardwareType::UAV);
+
+  ASSERT_EQ(uav_system.getStatus<JoystickVelocityControllerDroneConnector>(),
+            ControllerStatus::Active);
+
+  // run controller till it converges
+  while (uav_system.getStatus<JoystickVelocityControllerDroneConnector>() ==
+         ControllerStatus::Active) {
+    uav_system.runActiveController(HardwareType::UAV);
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+  };
+
+  // Verify controller status is completed
+  ASSERT_TRUE(bool(uav_system.getActiveControllerStatus(HardwareType::UAV)));
+
+  VelocityYaw vel_goal(
+      0.1, 0.1, -0.1,
+      -0.1 *
+          uav_system_config.joystick_velocity_controller_config()
+              .max_yaw_rate() *
+          0.02);
+
+  parsernode::common::quaddata sensor_data = uav_system.getUAVData();
+  ASSERT_NEAR(sensor_data.linvel.x, vel_goal.x, 1e-3);
+  ASSERT_NEAR(sensor_data.linvel.y, vel_goal.y, 1e-3);
+  ASSERT_NEAR(sensor_data.linvel.z, vel_goal.z, 1e-3);
+  ASSERT_NEAR(sensor_data.rpydata.z, vel_goal.yaw, 1e-3);
+}
+
+TEST(JoystickControlTests, TransitionGuardInvalidTest) {
+  QuadSimulator drone_hardware;
+  UAVSystem uav_system(
+      drone_hardware, UAVSystemConfig(),
+      std::shared_ptr<Sensor<VelocityYaw>>(new Sensor<VelocityYaw>()));
+  UAVLogicStateMachine sample_logic_state_machine(uav_system);
+  jcsa::JoystickControlGuard transition_guard_functor;
+  int dummy_start_state, dummy_target_state;
+
+  bool result = transition_guard_functor(jce::JoystickControlEvent(),
+                                         sample_logic_state_machine,
+                                         dummy_start_state, dummy_target_state);
+
+  ASSERT_FALSE(result);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ tests/state_machines/joystick_control_state_machine_tests.cpp
@@ -0,0 +1,112 @@
+#include <aerial_autonomy/joystick_control_events.h>
+#include <aerial_autonomy/state_machines/joystick_control_state_machine.h>
+#include <gtest/gtest.h>
+// Thread stuff
+#include <boost/optional/optional_io.hpp>
+#include <boost/thread/thread.hpp>
+// Quad Simulator
+#include <quad_simulator_parser/quad_simulator.h>
+// Guidance Sensor
+#include <aerial_autonomy/sensors/base_sensor.h>
+#include <aerial_autonomy/sensors/guidance.h>
+
+/**
+* @brief Namespace for UAV Simulator Hardware
+*/
+using namespace quad_simulator;
+
+/**
+* @brief  Namespace for joystick control events
+*/
+namespace jce = joystick_control_events;
+namespace be = uav_basic_events;
+
+class JoystickControlStateMachineTests : public ::testing::Test {
+public:
+  JoystickControlStateMachineTests() {
+    rpyt_config.set_kp(5.0);
+    rpyt_config.set_ki(0.01);
+
+    auto vel_ctlr_config = rpyt_config.mutable_velocity_controller_config();
+    auto tolerance = vel_ctlr_config->mutable_goal_velocity_tolerance();
+    tolerance->set_vx(1e-4);
+    tolerance->set_vy(1e-4);
+    tolerance->set_vz(1e-4);
+
+    drone_hardware.setTakeoffAltitude(2.0);
+
+    velocity_sensor =
+        std::shared_ptr<Sensor<VelocityYaw>>(new Guidance(drone_hardware));
+
+    uav_system.reset(new UAVSystem(drone_hardware, uav_system_config,
+                                   velocity_sensor, 0.02));
+    logic_state_machine.reset(
+        new JoystickControlStateMachine(boost::ref(*uav_system)));
+    logic_state_machine->start();
+    // Will switch to Landed state from manual control state
+    logic_state_machine->process_event(InternalTransitionEvent());
+  }
+
+protected:
+  std::unique_ptr<JoystickControlStateMachine> logic_state_machine;
+  std::unique_ptr<UAVSystem> uav_system;
+  UAVSystemConfig uav_system_config;
+  QuadSimulator drone_hardware;
+  std::shared_ptr<Sensor<VelocityYaw>> velocity_sensor;
+  RPYTBasedVelocityControllerConfig rpyt_config;
+
+  void GoToHoverFromLanded() {
+    drone_hardware.setBatteryPercent(100);
+    logic_state_machine->process_event(be::Takeoff());
+    logic_state_machine->process_event(InternalTransitionEvent());
+  }
+};
+
+TEST_F(JoystickControlStateMachineTests, InitialState) {
+  ASSERT_STREQ(pstate(*logic_state_machine), "Landed");
+}
+
+TEST_F(JoystickControlStateMachineTests, GoToJoystickControl) {
+  // First takeoff
+  GoToHoverFromLanded();
+  // Initialize Joystick Control
+  logic_state_machine->process_event(jce::JoystickControlEvent());
+  // Check controller status
+  ASSERT_EQ(uav_system->getStatus<JoystickVelocityControllerDroneConnector>(),
+            ControllerStatus::Active);
+  // Check that current state is Joyctick control state
+  ASSERT_STREQ(pstate(*logic_state_machine), "JoystickControlState");
+}
+
+TEST_F(JoystickControlStateMachineTests, Abort) {
+  // First takeoff
+  GoToHoverFromLanded();
+  // Initialize Joystick Control
+  logic_state_machine->process_event(jce::JoystickControlEvent());
+  // Check that current state is Joyctick control state
+  ASSERT_STREQ(pstate(*logic_state_machine), "JoystickControlState");
+  logic_state_machine->process_event(be::Abort());
+  ASSERT_STREQ(pstate(*logic_state_machine), "Hovering");
+}
+
+TEST_F(JoystickControlStateMachineTests, JoystickControlMode) {
+  // First takeoff
+  GoToHoverFromLanded();
+  // Initialize Joystick Control
+  logic_state_machine->process_event(jce::JoystickControlEvent());
+  // Check that current state is Joyctick control state
+  ASSERT_STREQ(pstate(*logic_state_machine), "JoystickControlState");
+  // Disable SDK
+  drone_hardware.flowControl(false);
+  logic_state_machine->process_event(InternalTransitionEvent());
+  ASSERT_STREQ(pstate(*logic_state_machine), "ManualControlState");
+  // Enable SDK
+  drone_hardware.flowControl(true);
+  logic_state_machine->process_event(InternalTransitionEvent());
+  ASSERT_STREQ(pstate(*logic_state_machine), "Hovering");
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
