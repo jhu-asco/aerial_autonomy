226f8a68a6c7b4fdd82c2bab4b594722e7e136c0
Matthew Sheckells <msheckells@gmail.com>
2017-11-30 12:49:05 -0500

    Add neg yaw test

--- tests/controllers/rpyt_based_position_controller_tests.cpp
+++ tests/controllers/rpyt_based_position_controller_tests.cpp
@@ -118,6 +118,10 @@ TEST_F(RPYTBasedPositionControllerTests, RunUntilConvergenceYaw) {
   runUntilConvergence(PositionYaw(-1.0, -1.0, 1.0, M_PI / 2.));
 }
 
+TEST_F(RPYTBasedPositionControllerTests, RunUntilConvergenceNegYaw) {
+  runUntilConvergence(PositionYaw(-1.0, -1.0, 1.0, -M_PI / 2.));
+}
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
