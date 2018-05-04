7ea84cab37a517ae2a2a9c88a5e40e9bbea14433
gbaraba1 <gbaraba1@jhu.edu>
2018-05-04 17:32:27 -0400

    State Machine Edits, still need to add tracker transition logic

--- include/aerial_autonomy/actions_guards/position_control_functors.h
+++ include/aerial_autonomy/actions_guards/position_control_functors.h
@@ -12,6 +12,45 @@
 
 namespace be = uav_basic_events;
 
+// Goals for search pattern (x,y,z,yaw)
+PositionYaw goal1(0.0,0.0,1.0,0.0);
+PositionYaw goal2(0.0,1.0,1.0,1.57);
+PositionYaw goal3(-1.0,1.0,1.0,3.14);
+PositionYaw goal4(-1.0,0.0,1.0,-1.57);
+
+
+template <class LogicStateMachineT>
+struct PositionControlTransitionActionGoal1Functor
+    : EventAgnosticActionFunctor<UAVSystem, LogicStateMachineT> {
+  void run(UAVSystem &robot_system) {
+    robot_system
+        .setGoal<DroneConnectorT, PositionYaw>(goal1);
+  }
+};
+template <class LogicStateMachineT>
+struct PositionControlTransitionActionGoal2Functor
+    : EventAgnosticActionFunctor<UAVSystem, LogicStateMachineT> {
+  void run(UAVSystem &robot_system) {
+    robot_system
+        .setGoal<DroneConnectorT, PositionYaw>(goal2);
+  }
+};
+template <class LogicStateMachineT>
+struct PositionControlTransitionActionGoal3Functor
+    : EventAgnosticActionFunctor<UAVSystem, LogicStateMachineT> {
+  void run(UAVSystem &robot_system) {
+    robot_system
+        .setGoal<DroneConnectorT, PositionYaw>(goal3);
+  }
+};
+template <class LogicStateMachineT>
+struct PositionControlTransitionActionGoal4Functor
+    : EventAgnosticActionFunctor<UAVSystem, LogicStateMachineT> {
+  void run(UAVSystem &robot_system) {
+    robot_system
+        .setGoal<DroneConnectorT, PositionYaw>(goal4);
+  }
+};
 /**
 * @brief Transition action to perform when going into position control mode
 *
