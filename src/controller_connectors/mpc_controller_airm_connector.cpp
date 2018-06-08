3a9cb7ed36e1b0c2f05a9867db42e1fc5a4dfd4a
Gowtham Garimella <ggarime1@jhu.edu>
2018-06-08 11:55:27 -0400

    Add an exponential filter on rpydot

--- src/controller_connectors/mpc_controller_airm_connector.cpp
+++ src/controller_connectors/mpc_controller_airm_connector.cpp
@@ -17,7 +17,8 @@ MPCControllerAirmConnector::MPCControllerAirmConnector(
       previous_measurement_time_(std::chrono::high_resolution_clock::now()),
       previous_measurements_initialized_(false),
       delay_buffer_size_(delay_buffer_size), private_controller_(controller),
-      use_perfect_time_diff_(false), perfect_time_diff_(0.02) {
+      use_perfect_time_diff_(false), perfect_time_diff_(0.02),
+      exponential_gain_(0.4) {
   clearCommandBuffers();
   DATA_HEADER("mpc_state_estimator") << "x"
                                      << "y"
@@ -87,6 +88,7 @@ void MPCControllerAirmConnector::setGoal(
   MPCControllerConnector<Eigen::VectorXd, Eigen::VectorXd>::setGoal(goal);
   VLOG(1) << "Clearing thrust estimator buffer";
   thrust_gain_estimator_.clearBuffer();
+  previous_measurements_initialized_ = false;
   clearCommandBuffers();
 }
 
@@ -164,16 +166,26 @@ bool MPCControllerAirmConnector::estimateStateAndParameters(
   } else {
     v = delta_rpy = Eigen::Vector3d::Zero();
     joint_velocities = Eigen::Vector2d::Zero();
+    filtered_joint_velocity_ = Eigen::Vector2d::Zero();
+    filtered_rpydot_ = Eigen::Vector3d::Zero();
     previous_measurements_initialized_ = true;
   }
+  // Update filtered velocities:
+  // filtered_rpydot_ = delta_rpy/dt;
+  filtered_rpydot_ = exponential_gain_ * filtered_rpydot_ +
+                     (1 - exponential_gain_) * (delta_rpy / dt);
+  // filtered_joint_velocity_  = exponential_gain_*filtered_joint_velocity_ +
+  // (1-exponential_gain_)*joint_velocities;
+  filtered_joint_velocity_ =
+      joint_velocities; //// If using filter then uncomment above line
   // Fill state
   current_state.segment<3>(0) = p;
   current_state.segment<3>(3) = rpy;
   current_state.segment<3>(6) = v;
-  current_state.segment<3>(9) = delta_rpy / dt;
+  current_state.segment<3>(9) = filtered_rpydot_;
   current_state.segment<3>(12) = rpy_command_buffer_.front();
   current_state.segment<2>(15) = joint_angles_vec;
-  current_state.segment<2>(17) = joint_velocities;
+  current_state.segment<2>(17) = filtered_joint_velocity_;
   current_state.segment<2>(19) = previous_joint_commands_;
   // Fill previous measurements and time
   previous_measurements_.segment<6>(0) = current_state.segment<6>(0);
