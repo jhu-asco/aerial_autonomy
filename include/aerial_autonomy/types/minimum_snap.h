c5c0b1b0ef398d08e766de41c023e4f6ff4e1129
yorgoon <soowk311@gmail.com>
2018-07-17 15:01:01 -0400

    Unit tested

--- include/aerial_autonomy/types/minimum_snap.h
+++ include/aerial_autonomy/types/minimum_snap.h
@@ -1,38 +1,40 @@
 #pragma once
-#include "aerial_autonomy/types/reference_trajectory.h"
 #include "aerial_autonomy/types/particle_state.h"
+#include "aerial_autonomy/types/reference_trajectory.h"
 #include "aerial_autonomy/types/snap.h"
 
-#include <utility>
 #include <Eigen/Dense>
-#include <vector>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <utility>
+#include <vector>
 
 using namespace Eigen;
 
-class MinimumSnapReferenceTrajectory: public ReferenceTrajectory<ParticleState, Snap>
-{
+class MinimumSnapReferenceTrajectory
+    : public ReferenceTrajectory<ParticleState, Snap> {
 public:
   /*Constructor*/
-  MinimumSnapReferenceTrajectory
-  (const int r_in, const Ref<const VectorXd>& tau_vec_in, const Ref<const MatrixXd>& path_in) // normal non-default constructor
-  : r(r_in), tau_vec(tau_vec_in), path(path_in)
-  {
+  MinimumSnapReferenceTrajectory(
+      const int r_in, const Ref<const VectorXd> &tau_vec_in,
+      const Ref<const MatrixXd> &path_in) // normal non-default constructor
+      : r(r_in),
+        tau_vec(tau_vec_in),
+        path(path_in) {
     Init(); // Separate function
   }
 
-  //void Init(int r_in, const Ref<const VectorXd>& tau_vec_in, const Ref<const MatrixXd>& path_in);
+  // void Init(int r_in, const Ref<const VectorXd>& tau_vec_in, const Ref<const
+  // MatrixXd>& path_in);
   // MinimumSnapReferenceTrajectory Member functions
-  void Init()
-  {
-    n = 2*r + 1; // order of the polynomial (ex. for minimum snap, 9th poly)
+  void Init() {
+    n = 2 * r + 1; // order of the polynomial (ex. for minimum snap, 9th poly)
     m = tau_vec.size(); // Number of segments
     A = augA();
     C = permutC();
     Q = augQ();
     bOpt = bOptimized();
-    P = A.lu().solve(C.transpose())*bOpt;
+    P = A.lu().solve(C.transpose()) * bOpt;
     ts_eigen = cumsumEigen();
     ts = EigenToVector(ts_eigen);
   }
@@ -43,36 +45,37 @@ public:
 
   MatrixXd getEqualA(double t_tau) const { return equalA(t_tau); }
 
-  std::vector<double> EigenToVector(const VectorXd& vector_eigen) {
-    std::vector<double> vector_std(ts_eigen.data(), ts_eigen.data() + ts_eigen.rows() * ts_eigen.cols());
+  std::vector<double> EigenToVector(const Ref<const VectorXd> &vector_eigen) {
+    std::vector<double> vector_std(
+        ts_eigen.data(), ts_eigen.data() + ts_eigen.rows() * ts_eigen.cols());
     return vector_std;
   }
 
-  std::pair<ParticleState, Snap> atTime(double t) const
-  {
-    //if (this->ts.empty() || t < this->ts.front() || t > this->ts.back()) {
-    //  throw std::out_of_range("Accessed reference trajectory out of bounds");
-    //}
-    // find interator
+  std::pair<ParticleState, Snap> atTime(double t) const {
+    if (ts.empty() || t < ts.front() || t > ts.back()) {
+      throw std::out_of_range("Accessed reference trajectory out of bounds");
+    }
+
     auto closest_t = std::lower_bound(ts.begin(), ts.end(), t);
     int i = closest_t - ts.begin();
-    double t_tau = t - ts[i-1];
+    double t_tau = t - ts[i - 1];
     MatrixXd P = getP();
     MatrixXd states_eigen;
     if (i == 0) {
-      states_eigen = equalA(t_tau).bottomRows(5)*P.middleRows(0, 10);
+      states_eigen = equalA(t_tau).bottomRows(5) * P.middleRows(0, 10);
     } else {
-      states_eigen = equalA(t_tau).bottomRows(5)*P.middleRows(10*(i-1), 10);
+      states_eigen =
+          equalA(t_tau).bottomRows(5) * P.middleRows(10 * (i - 1), 10);
     }
 
     // Type conversion
-    Position p(states_eigen(0,0), states_eigen(0,1), states_eigen(0,2));
-    Velocity v(states_eigen(1,0), states_eigen(1,1), states_eigen(1,2));
-    Acceleration a(states_eigen(2,0), states_eigen(2,1), states_eigen(2,2));
-    Jerk j(states_eigen(3,0), states_eigen(3,1), states_eigen(3,2));
-    Snap snap(states_eigen(4,0), states_eigen(4,1), states_eigen(4,2));
+    Position p(states_eigen(0, 0), states_eigen(0, 1), states_eigen(0, 2));
+    Velocity v(states_eigen(1, 0), states_eigen(1, 1), states_eigen(1, 2));
+    Acceleration a(states_eigen(2, 0), states_eigen(2, 1), states_eigen(2, 2));
+    Jerk j(states_eigen(3, 0), states_eigen(3, 1), states_eigen(3, 2));
+    Snap snap(states_eigen(4, 0), states_eigen(4, 1), states_eigen(4, 2));
 
-    return std::pair<ParticleState, Snap> (ParticleState(p, v, a, j), snap);
+    return std::pair<ParticleState, Snap>(ParticleState(p, v, a, j), snap);
   }
 
 private:
@@ -85,7 +88,7 @@ private:
   MatrixXd A;
   MatrixXd C;
   MatrixXd Q;
-  //VectorXd bfixed;
+  // VectorXd bfixed;
   MatrixXd bOpt;
   MatrixXd P;
   VectorXd ts_eigen;
@@ -98,7 +101,8 @@ private:
   // member functions
 
   /* Equality matrix, A */
-  // Equality matrix, A is a mapping matrix from the coefficents of the polynomial
+  // Equality matrix, A is a mapping matrix from the coefficents of the
+  // polynomial
   // , [p0 p1 p2 ... p9] to the equality constraint, [b0, bt]
   // b0: start point of the polynomial; [pos, vel, accel, jerk, snap]
   // bt: end point of the polynomial
@@ -110,31 +114,33 @@ private:
   // minimum snap, r = 4)
   // Output:
   // A = equality matrix
-  //MatrixXd equalA(double tau) const;
-  MatrixXd equalA(double tau) const
-  {
-    MatrixXd A0, Atau1, Atau; A0.setZero(r+1,n+1); Atau.setZero(r+1,n+1);
-    MatrixXd A; A.setZero(n+1,n+1);
-    A0(0,0) = 1;
+  // MatrixXd equalA(double tau) const;
+  MatrixXd equalA(double tau) const {
+    MatrixXd A0, Atau1, Atau;
+    A0.setZero(r + 1, n + 1);
+    Atau.setZero(r + 1, n + 1);
+    MatrixXd A;
+    A.setZero(n + 1, n + 1);
+    A0(0, 0) = 1;
     for (int i = 0; i <= r; i++) {
       for (int j = 0; j <= n; j++) {
         if (i == 0) {
           if (j >= i) {
-            Atau(i,j) = pow(tau, j-i);
+            Atau(i, j) = pow(tau, j - i);
           }
         } else {
-          VectorXd m = VectorXd::LinSpaced(i, j, j-(i-1));
+          VectorXd m = VectorXd::LinSpaced(i, j, j - (i - 1));
           if (j == i) {
-            A0(i,j) = m.prod();
+            A0(i, j) = m.prod();
           }
           if (j >= i) {
-            Atau(i,j) = m.prod()*pow(tau, j-i);
+            Atau(i, j) = m.prod() * pow(tau, j - i);
           }
         }
       }
     }
-    A.block(0,0,r+1,n+1) = A0;
-    A.block(r+1,0,r+1,n+1) = Atau;
+    A.block(0, 0, r + 1, n + 1) = A0;
+    A.block(r + 1, 0, r + 1, n + 1) = Atau;
     return A;
   }
   /* Augmented equality matrices, A1, A2, A3, ... */
@@ -144,18 +150,18 @@ private:
   // minimum snap, r = 4)
   // Output:
   // A = augmented equality matrix
-  MatrixXd augA()
-  {
-    MatrixXd A; A.setZero((n+1)*m, (n+1)*m);
-    for (int i = 0; i < m ; i++)
-    {
-      A.block((n+1)*i, (n+1)*i, (n+1), (n+1)) = equalA(tau_vec(i));
+  MatrixXd augA() {
+    MatrixXd A;
+    A.setZero((n + 1) * m, (n + 1) * m);
+    for (int i = 0; i < m; i++) {
+      A.block((n + 1) * i, (n + 1) * i, (n + 1), (n + 1)) = equalA(tau_vec(i));
     }
     /* Continuity constraint */
-    // Enforce continuity (velocity, accel, jerk, snap) of the two adjacent polynomials
-    for (int i = 0; i < m-1; i++)
-    {
-      A.block((n+1)*i+6, (n+1)*(i+1), 4, (n+1)) = -A.block((n+1)*(i+1)+1, (n+1)*(i+1), 4, (n+1));
+    // Enforce continuity (velocity, accel, jerk, snap) of the two adjacent
+    // polynomials
+    for (int i = 0; i < m - 1; i++) {
+      A.block((n + 1) * i + 6, (n + 1) * (i + 1), 4, (n + 1)) =
+          -A.block((n + 1) * (i + 1) + 1, (n + 1) * (i + 1), 4, (n + 1));
     }
     return A;
   }
@@ -166,19 +172,17 @@ private:
   // minimum snap, r = 4)
   // Output:
   // Q = cost matrix
-  MatrixXd costQ(double tau)
-  {
-    MatrixXd Q; Q.setZero(n+1,n+1);
-    for (int i = 0; i < Q.rows(); i++)
-    {
-      for (int j = 0; j < Q.cols(); j++)
-      {
-        if (i >= r && j >= r)
-        {
-          VectorXd i_m = VectorXd::LinSpaced(r, i, i-(r-1));
-          VectorXd j_m = VectorXd::LinSpaced(r, j, j-(r-1));
+  MatrixXd costQ(double tau) {
+    MatrixXd Q;
+    Q.setZero(n + 1, n + 1);
+    for (int i = 0; i < Q.rows(); i++) {
+      for (int j = 0; j < Q.cols(); j++) {
+        if (i >= r && j >= r) {
+          VectorXd i_m = VectorXd::LinSpaced(r, i, i - (r - 1));
+          VectorXd j_m = VectorXd::LinSpaced(r, j, j - (r - 1));
           VectorXd ij_m = i_m.cwiseProduct(j_m);
-          Q(i,j) = 2*ij_m.prod()*pow(tau, (i+j-2*r+1))/(i+j-2*r+1);
+          Q(i, j) = 2 * ij_m.prod() * pow(tau, (i + j - 2 * r + 1)) /
+                    (i + j - 2 * r + 1);
         }
       }
     }
@@ -191,12 +195,11 @@ private:
   // minimum snap, r = 4)
   // Output:
   // Q = augmented cost matrix
-  MatrixXd augQ()
-  {
-    MatrixXd Q; Q.setZero((n+1)*m, (n+1)*m);
-    for (int i = 0; i < m ; i++)
-    {
-     Q.block((n+1)*i, (n+1)*i, (n+1), (n+1)) = costQ(tau_vec(i));
+  MatrixXd augQ() {
+    MatrixXd Q;
+    Q.setZero((n + 1) * m, (n + 1) * m);
+    for (int i = 0; i < m; i++) {
+      Q.block((n + 1) * i, (n + 1) * i, (n + 1), (n + 1)) = costQ(tau_vec(i));
     }
     return Q;
   }
@@ -208,33 +211,32 @@ private:
   // minimum snap, r = 4)
   // Output:
   // C = permutation matrix
-  MatrixXd permutC()
-  {
-    int n = 2*r + 1;
+  MatrixXd permutC() {
+    int n = 2 * r + 1;
     int m = tau_vec.size();
-    VectorXd idx; idx.setZero(4*(m-1));
-    for (int i = 1; i <= m-1; i++)
-    {
-      VectorXd idx_tmp = VectorXd::LinSpaced(4, (n+1)*i+1, (n+1)*i+4);
-      idx.segment(4*(i-1), 4) = idx_tmp;
+    VectorXd idx;
+    idx.setZero(4 * (m - 1));
+    for (int i = 1; i <= m - 1; i++) {
+      VectorXd idx_tmp =
+          VectorXd::LinSpaced(4, (n + 1) * i + 1, (n + 1) * i + 4);
+      idx.segment(4 * (i - 1), 4) = idx_tmp;
     }
-    MatrixXd C; C.setZero((n+1)*m, (n+1)*m);
-    MatrixXd C_app; C_app.setZero(4*(m-1), (n+1)*m);
+    MatrixXd C;
+    C.setZero((n + 1) * m, (n + 1) * m);
+    MatrixXd C_app;
+    C_app.setZero(4 * (m - 1), (n + 1) * m);
     int C_row_idx = 0;
     int C_app_row_idx = 0;
-    for (int i = 0; i < (n+1)*m; i++)
-    {
-      if ((idx.array() == i).any())
-      {
+    for (int i = 0; i < (n + 1) * m; i++) {
+      if ((idx.array() == i).any()) {
         C_app(C_app_row_idx, i) = 1;
         C_app_row_idx++;
-      } else
-      {
+      } else {
         C(C_row_idx, i) = 1;
         C_row_idx++;
       }
     }
-    C.bottomRows(4*(m-1)) = C_app;
+    C.bottomRows(4 * (m - 1)) = C_app;
     return C;
   }
   /* Known (fixed) endpoint derivatives, b */
@@ -244,15 +246,15 @@ private:
   // minimum snap, r = 4)
   // Output:
   // b = known endpoint derivatives
-  MatrixXd bFixed()
-  {
-    //int idx_free = 4*(m-1); // # of unknowns (subject to optimization)
-    MatrixXd b; b.setZero((n+1)*m, path.cols());
+  MatrixXd bFixed() {
+    // int idx_free = 4*(m-1); // # of unknowns (subject to optimization)
+    MatrixXd b;
+    b.setZero((n + 1) * m, path.cols());
     b.topRows(1) = path.topRows(1);
-    b.row((r+1)+(n+1)*(m-1)) = path.bottomRows(1);
-    for (int i = 0; i < path.rows()-2; i++) {
-      b.row((r+1)+(n+1)*i) = path.row(i+1);
-      b.row((2*r+2)+(n+1)*i) = path.row(i+1);
+    b.row((r + 1) + (n + 1) * (m - 1)) = path.bottomRows(1);
+    for (int i = 0; i < path.rows() - 2; i++) {
+      b.row((r + 1) + (n + 1) * i) = path.row(i + 1);
+      b.row((2 * r + 2) + (n + 1) * i) = path.row(i + 1);
     }
     return b;
   }
@@ -264,50 +266,47 @@ private:
   // minimum snap, r = 4)
   // Output:
   // b_Opt = optimized endpoint derivatives
-  MatrixXd bOptimized()
-  {
-    //int m = bfixed.rows()/(n+1);
-    int m = bFixed().rows()/(n+1);
-    //VectorXd b_Opt = C*bfixed;
-    MatrixXd b_Opt = C*bFixed();
-    MatrixXd bF = b_Opt.topRows((n+1)*m - 4*(m-1));
-    MatrixXd R = C*A.transpose().lu().solve(Q)*A.lu().solve(C.transpose());
-    MatrixXd Rfp = R.topRightCorner((n+1)*m - 4*(m-1), 4*(m-1));
-    MatrixXd Rpp = R.bottomRightCorner(4*(m-1), 4*(m-1));
-    MatrixXd bP_Opt = -Rpp.lu().solve(Rfp.transpose())*bF;
-    b_Opt.bottomRows(4*(m-1)) = bP_Opt;
+  MatrixXd bOptimized() {
+    // int m = bfixed.rows()/(n+1);
+    int m = bFixed().rows() / (n + 1);
+    // VectorXd b_Opt = C*bfixed;
+    MatrixXd b_Opt = C * bFixed();
+    MatrixXd bF = b_Opt.topRows((n + 1) * m - 4 * (m - 1));
+    MatrixXd R = C * A.transpose().lu().solve(Q) * A.lu().solve(C.transpose());
+    MatrixXd Rfp = R.topRightCorner((n + 1) * m - 4 * (m - 1), 4 * (m - 1));
+    MatrixXd Rpp = R.bottomRightCorner(4 * (m - 1), 4 * (m - 1));
+    MatrixXd bP_Opt = -Rpp.lu().solve(Rfp.transpose()) * bF;
+    b_Opt.bottomRows(4 * (m - 1)) = bP_Opt;
     return b_Opt;
   }
 
-  VectorXd cumsumEigen()
-  {
-    //VectorXd ts_eigen;
-    ts_eigen.setZero(m+1);
-    for (int i = 1; i < m+1; i++) {
+  VectorXd cumsumEigen() {
+    // VectorXd ts_eigen;
+    ts_eigen.setZero(m + 1);
+    for (int i = 1; i < m + 1; i++) {
       /* code */
-      //std::cout << "/* message */" << tau_vec.head(i) << '\n';
+      // std::cout << "/* message */" << tau_vec.head(i) << '\n';
       ts_eigen(i) = tau_vec.head(i).sum();
     }
     return ts_eigen;
   }
-
 };
 
-template<typename M>
-M load_csv (const std::string & path) {
-    std::ifstream indata;
-    indata.open(path);
-    std::string line;
-    std::vector<double> values;
-    uint rows = 0;
-    while (std::getline(indata, line)) {
-        std::stringstream lineStream(line);
-        std::string cell;
-        while (std::getline(lineStream, cell, ',')) {
-            values.push_back(std::stod(cell));
-        }
-        ++rows;
+template <typename M> M load_csv(const std::string &path) {
+  std::ifstream indata;
+  indata.open(path);
+  std::string line;
+  std::vector<double> values;
+  uint rows = 0;
+  while (std::getline(indata, line)) {
+    std::stringstream lineStream(line);
+    std::string cell;
+    while (std::getline(lineStream, cell, ',')) {
+      values.push_back(std::stod(cell));
     }
-    return Map<const Matrix<typename M::Scalar, M::RowsAtCompileTime, M::ColsAtCompileTime, RowMajor>>
-    (values.data(), rows, values.size()/rows);
+    ++rows;
+  }
+  return Map<const Matrix<typename M::Scalar, M::RowsAtCompileTime,
+                          M::ColsAtCompileTime, RowMajor>>(
+      values.data(), rows, values.size() / rows);
 };
--- tests/types/minimum_snap_trajectory_tests.cpp
+++ tests/types/minimum_snap_trajectory_tests.cpp
@@ -1,45 +1,84 @@
 #include <aerial_autonomy/types/minimum_snap.h>
 #include <gtest/gtest.h>
 
+void ASSERT_VEC_NEAR(const MatrixXd &state1,
+                     const std::pair<ParticleState, Snap> &state2,
+                     double tol = 1e-7) {
+  // ParticleState state2_particle = std::get<0>(state2);
+  ASSERT_NEAR(state1(0, 0), std::get<0>(state2).p.x, tol);
+  ASSERT_NEAR(state1(0, 1), std::get<0>(state2).p.y, tol);
+  ASSERT_NEAR(state1(0, 2), std::get<0>(state2).p.z, tol);
 
-void ASSERT_VEC_NEAR(const MatrixXd &state1, const std::pair<ParticleState, Snap> &state2, double tol = 1e-5) {
-  //ParticleState state2_particle = std::get<0>(state2);
-  ASSERT_NEAR(state1(0,0), std::get<0>(state2).p.x, tol);
-  ASSERT_NEAR(state1(0,1), std::get<0>(state2).p.y, tol);
-  ASSERT_NEAR(state1(0,2), std::get<0>(state2).p.z, tol);
+  ASSERT_NEAR(state1(1, 0), std::get<0>(state2).v.x, tol);
+  ASSERT_NEAR(state1(1, 1), std::get<0>(state2).v.y, tol);
+  ASSERT_NEAR(state1(1, 2), std::get<0>(state2).v.z, tol);
 
-  ASSERT_NEAR(state1(1,0), std::get<0>(state2).v.x, tol);
-  ASSERT_NEAR(state1(1,1), std::get<0>(state2).v.y, tol);
-  ASSERT_NEAR(state1(1,2), std::get<0>(state2).v.z, tol);
+  ASSERT_NEAR(state1(2, 0), std::get<0>(state2).a.x, tol);
+  ASSERT_NEAR(state1(2, 1), std::get<0>(state2).a.y, tol);
+  ASSERT_NEAR(state1(2, 2), std::get<0>(state2).a.z, tol);
 
-  ASSERT_NEAR(state1(2,0), std::get<0>(state2).a.x, tol);
-  ASSERT_NEAR(state1(2,1), std::get<0>(state2).a.y, tol);
-  ASSERT_NEAR(state1(2,2), std::get<0>(state2).a.z, tol);
+  ASSERT_NEAR(state1(3, 0), std::get<0>(state2).j.x, tol);
+  ASSERT_NEAR(state1(3, 1), std::get<0>(state2).j.y, tol);
+  ASSERT_NEAR(state1(3, 2), std::get<0>(state2).j.z, tol);
 
-  ASSERT_NEAR(state1(3,0), std::get<0>(state2).j.x, tol);
-  ASSERT_NEAR(state1(3,1), std::get<0>(state2).j.y, tol);
-  ASSERT_NEAR(state1(3,2), std::get<0>(state2).j.z, tol);
-
-  ASSERT_NEAR(state1(4,0), std::get<1>(state2).x, tol);
-  ASSERT_NEAR(state1(4,1), std::get<1>(state2).y, tol);
-  ASSERT_NEAR(state1(4,2), std::get<1>(state2).z, tol);
+  ASSERT_NEAR(state1(4, 0), std::get<1>(state2).x, tol);
+  ASSERT_NEAR(state1(4, 1), std::get<1>(state2).y, tol);
+  ASSERT_NEAR(state1(4, 2), std::get<1>(state2).z, tol);
 }
 
-TEST(MinimumSnapReferenceTrajectory, TimeInMiddle) {
+TEST(DiscreteReferenceTrajectoryInterpolate, TimeTooSmall) {
   int r = 4;
+  VectorXd tau_vec(2);
+  tau_vec << 1, 1;
+  MatrixXd path(3, 3);
+  path << 0, 0, 0, 1, 1, 1, 2, 2, 2;
+  const MinimumSnapReferenceTrajectory ref(r, tau_vec, path);
+  ASSERT_THROW(ref.atTime(-1), std::out_of_range);
+}
 
-  MatrixXd path_matlab = load_csv<MatrixXd>("/home/soowon/MATLAB/workspace/path.csv");
-  MatrixXd tau_vec_matlab = load_csv<MatrixXd>("/home/soowon/MATLAB/workspace/tau_vec.csv");
-  MatrixXd states_matlab = load_csv<MatrixXd>("/home/soowon/MATLAB/workspace/states.csv");
-  std::ifstream file("/home/soowon/MATLAB/workspace/atTimet.csv");
-  std::string tt;
-  std::getline(file, tt);
-  double t_matlab = std::stod(tt);
+TEST(DiscreteReferenceTrajectoryInterpolate, TimeTooLarge) {
+  int r = 4;
+  VectorXd tau_vec(2);
+  tau_vec << 1, 1;
+  MatrixXd path(3, 3);
+  path << 0, 0, 0, 1, 1, 1, 2, 2, 2;
+  const MinimumSnapReferenceTrajectory ref(r, tau_vec, path);
+  ASSERT_THROW(ref.atTime(3), std::out_of_range);
+}
+
+TEST(MinimumSnapReferenceTrajectory, MATLAB) {
+  int r = 4;
+  MatrixXd path_matlab =
+      load_csv<MatrixXd>("/home/soowon/MATLAB/workspace/path_matlab.csv");
+  MatrixXd tau_vec_matlab = load_csv<MatrixXd>(
+      "/home/soowon/MATLAB/workspace/tau_vec_matlab.csv"); // vec -> Mat...
+  MatrixXd states_matlab =
+      load_csv<MatrixXd>("/home/soowon/MATLAB/workspace/states_matlab.csv");
+  // std::ifstream file("/home/soowon/MATLAB/workspace/atTimet.csv");
+  // std::string tt;
+  // std::getline(file, tt);
+  // double t_matlab = std::stod(tt);
+  MatrixXd t_matlab =
+      load_csv<MatrixXd>("/home/soowon/MATLAB/workspace/t_matlab.csv");
 
   const MinimumSnapReferenceTrajectory ref(r, tau_vec_matlab, path_matlab);
-  auto ref_t = ref.atTime(t_matlab);
+  for (int i = 0; i < t_matlab.rows(); i++) {
+    auto ref_t = ref.atTime(t_matlab(i));
+    ASSERT_VEC_NEAR(states_matlab.middleRows(5 * i, 5), ref_t);
+  }
+}
 
-  ASSERT_VEC_NEAR(states_matlab, ref_t);
+TEST(MinimumSnapReferenceTrajectory, TimeAtStart) {
+  int r = 4;
+  VectorXd tau_vec(2);
+  tau_vec << 1, 1;
+  MatrixXd path(3, 3);
+  path << 0, 0, 0, 1, 1, 1, 2, 2, 2;
+  const MinimumSnapReferenceTrajectory ref(r, tau_vec, path);
+  auto ref_t = ref.atTime(0);
+  ASSERT_NEAR(std::get<0>(ref_t).p.x, 0, 1e-7);
+  ASSERT_NEAR(std::get<0>(ref_t).p.y, 0, 1e-7);
+  ASSERT_NEAR(std::get<0>(ref_t).p.z, 0, 1e-7);
 }
 
 int main(int argc, char **argv) {
