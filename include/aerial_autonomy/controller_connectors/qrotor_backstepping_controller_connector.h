9a9ff3ee1975795f14f35ece6876d410275e2884
yorgoon <soowk311@gmail.com>
2018-07-24 21:51:36 -0400

    header file for a connector for backstepping controller created

---
+++ include/aerial_autonomy/controller_connectors/qrotor_backstepping_controller_connector.h
@@ -0,0 +1,35 @@
+#pragma once
+// #include
+// "aerial_autonomy/controller_connectors/abstract_constraint_generator.h"
+// #include "aerial_autonomy/controller_connectors/abstract_control_selector.h"
+// #include "aerial_autonomy/controller_connectors/base_controller_connector.h"
+// #include "aerial_autonomy/controllers/qrotor_backstepping_controller.h"
+// #include "aerial_autonomy/estimators/state_estimator.h"
+
+// #include <chrono>
+
+/**
+ * @brief A controller connector for trajectory-tracking backstepping controller
+ * for a quadrotor.
+ * Based on <Kobilarov, "Trajectory tracking of a class of underactuated systems
+ * with
+ * external disturbances", American Control Conference, 2013>
+ */
+// template <class StateT, class ControlT> do we need templated?
+// class QrotorBacksteppingControllerConnector
+// : public ControllerConnector<
+// SensorDataType = std::pair<double, QrotorBacksteppingState>,?
+// GoalType = std::shared_ptr<ReferenceTrajectory<ParticleState, Snap>>,?
+// ControlType QrotorBacksteppingControl> {?
+/**
+* @brief Parent controller connector
+*/
+// using BaseConnector =
+//     ControllerConnector<SensorDataType = std::pair<double,
+//     QrotorBacksteppingState>,?
+//                         GoalType =
+//                         std::shared_ptr<ReferenceTrajectory<ParticleState,
+//                         Snap>>,?
+//                         ControlType QrotorBacksteppingControl>>?
+
+// public:
