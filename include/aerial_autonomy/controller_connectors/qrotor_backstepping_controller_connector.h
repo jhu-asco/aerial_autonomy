c8f561d970c7937cb070336b3b0f89a82d0f21b2
yorgoon <soowk311@gmail.com>
2018-08-01 17:02:16 -0400

    Add overall outline of the connector

--- include/aerial_autonomy/controller_connectors/qrotor_backstepping_controller_connector.h
+++ include/aerial_autonomy/controller_connectors/qrotor_backstepping_controller_connector.h
@@ -1,22 +1,22 @@
 #pragma once
 
 #include "aerial_autonomy/controller_connectors/base_controller_connector.h"
-#include "aerial_autonomy/controllers/rpyt_based_position_controller.h"
+#include "aerial_autonomy/controllers/qrotor_backstepping_controller.h"
 #include "aerial_autonomy/estimators/thrust_gain_estimator.h"
-#include "aerial_autonomy/types/position_yaw.h"
 #include "aerial_autonomy/types/roll_pitch_yawrate_thrust.h"
-#include "aerial_autonomy/types/velocity_yaw_rate.h"
+#include "qrotor_backstepping_controller_config.pb.h"
 
+#include <Eigen/Dense>
 #include <chrono>
 #include <parsernode/parser.h>
 
 /**
- * @brief A controller connector for trajectory-tracking backstepping controller
- * for a quadrotor.
- * Based on <Kobilarov, "Trajectory tracking of a class of underactuated systems
- * with
- * external disturbances", American Control Conference, 2013>
- */
+* @brief A controller connector for trajectory-tracking backstepping controller
+* for a quadrotor.
+* Based on <Kobilarov, "Trajectory tracking of a class of underactuated systems
+* with
+* external disturbances", American Control Conference, 2013>
+*/
 class QrotorBacksteppingControllerConnector
     : public ControllerConnector<
           std::pair<double, QrotorBacksteppingState>, // SensorDataType
@@ -31,53 +31,119 @@ public:
   QrotorBacksteppingControllerConnector(
       parsernode::Parser &drone_hardware,
       QrotorBacksteppingController &controller,
-      ThrustGainEstimator &thrust_gain_estimator)
+      ThrustGainEstimator &thrust_gain_estimator,
+      QrotorBacksteppingControllerConfig config,
+      std::chrono::duration<double> controller_timer_duration)
       : ControllerConnector(controller, ControllerGroup::UAV),
         drone_hardware_(drone_hardware),
         thrust_gain_estimator_(thrust_gain_estimator),
         private_reference_controller_(controller),
-        t_0_(std::chrono::high_resolution_clock::now()) {}
+        t_0_(std::chrono::high_resolution_clock::now()), config_(config),
+        m_(config_.mass()), g_(config_.acc_gravity()),
+        dt_(controller_timer_duration.count()) {
+    J_ << config_.jxx(), config_.jxy(), config_.jxz(), config_.jyx(),
+        config_.jyy(), config_.jyz(), config_.jzx(), config_.jzy(),
+        config_.jzz();
+  }
 
   /**
-   * @brief set goal to controller and clear estimator buffer
-   *
-   * @param goal empty goal
-   */
+  * @brief set goal to controller and clear estimator buffer
+  *
+  * @param goal empty goal
+  */
   void setGoal(std::shared_ptr<ReferenceTrajectory<ParticleState, Snap>> goal);
+
+  void setThrust(double thrust) { thrust_ = thrust; }
+  double getThrust() { return thrust_; }
+  void setThrustDot(double thrust_dot) { thrust_dot_ = thrust_dot; }
+  double getThrustDot() { return thrust_dot_; }
+  // QrotorBacksteppingControllerConfig getConfig() { return config_; }
 
 protected:
   /**
-   * @brief extracts position and velocity data from UAV to compute appropriate
-   * rpyt
-   * commands
-   *
-   * @param sensor_data Current position and velocity of UAV
-   *
-   * @return true if sensor data can be extracted
-   */
+  * @brief extracts position and velocity data from UAV to compute appropriate
+  * rpyt
+  * commands
+  *
+  * @param sensor_data Current position and velocity of UAV
+  *
+  * @return true if sensor data can be extracted
+  */
   virtual bool
   extractSensorData(std::pair<double, QrotorBacksteppingState> &sensor_data);
 
   /**
-   * @brief  Send rpyt commands to hardware
-   *
-   * @param controls rpyt commands to send to UAV
-   */
-  virtual void sendControllerCommands(QrotorBacksteppingControl control);
+  * @brief  Send rpyt commands to hardware
+  *
+  * @param controls rpyt commands to send to UAV
+  */
+  virtual void sendControllerCommands(
+      QrotorBacksteppingControl control,
+      std::pair<double, QrotorBacksteppingState> &sensor_data);
+
+  /**
+  * @brief  Mapping from body angular velocity to local rpydot
+  *
+  * @param omega body angular velocity
+  * @param rpy current rpy from sensor data
+  *
+  * @return eigen vector3d, rpydot
+  */
+  Eigen::Vector3d omegaToRpyDot(const Eigen::Vector3d &omega,
+                                const Eigen::Vector3d &rpy);
 
 private:
+  /**
+  * @brief Controller config
+  */
+  QrotorBacksteppingControllerConfig config_;
+  /**
+  * @brief Base class typedef to simplify code
+  */
+  using BaseClass = ControllerConnector<
+      std::pair<double, QrotorBacksteppingState>,
+      std::shared_ptr<ReferenceTrajectory<ParticleState, Snap>>,
+      QrotorBacksteppingControl>;
   /**
   * @brief Quad hardware to send commands
   */
   parsernode::Parser &drone_hardware_;
   /**
-   * @brief Estimator for finding the gain between joystick thrust command and
-   * the acceleration in body z direction
-   */
+  * @brief Estimator for finding the gain between joystick thrust command and
+  * the acceleration in body z direction
+  */
   ThrustGainEstimator &thrust_gain_estimator_;
   /**
-   * @brief Internal reference to controller that is connected by this class
-   */
+  * @brief Internal reference to controller that is connected by this class
+  */
   QrotorBacksteppingController &private_reference_controller_;
-
+  /**
+  * @brief time at start
+  */
   std::chrono::time_point<std::chrono::high_resolution_clock> t_0_;
+  /**
+  * @brief Variable to store current thrust
+  */
+  double thrust_;
+  /**
+  * @brief Variable to store current thrust_dot
+  */
+  double thrust_dot_;
+  /**
+  * @brief Mass
+  */
+  double m_;
+  /**
+  * @brief Gravity
+  */
+  double g_;
+  /**
+  * @brief Moment of inertia matrix
+  */
+  Eigen::Matrix3d J_;
+  /**
+  * @brief Time duration for numerical integration
+  */
+  double dt_;
+
+  parsernode::common::quaddata data_;
--- src/controller_connectors/qrotor_backstepping_controller_connector.cpp
+++ src/controller_connectors/qrotor_backstepping_controller_connector.cpp
@@ -2,11 +2,13 @@
 
 bool QrotorBacksteppingControllerConnector::extractSensorData(
     std::pair<double, QrotorBacksteppingState> &sensor_data) {
-  parsernode::common::quaddata data;
-  drone_hardware_.getquaddata(data);
+  // parsernode::common::quaddata data;
+  drone_hardware_.getquaddata(data_);
 
-  double current_time = std::chrono::high_resolution_clock::now() - t_0_;
-
+  // double current_time = std::chrono::high_resolution_clock::now() - t_0_;
+  std::chrono::duration<double> time_duration = duration_cast<duration<double>>(
+      std::chrono::high_resolution_clock::now() - t_0_);
+  double current_time = time_duration.count();
   QrotorBacksteppingState qrotor_backstepping_state;
 
   /* Objective
@@ -16,45 +18,90 @@ bool QrotorBacksteppingControllerConnector::extractSensorData(
   * ex)pose(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0, 0, 0))
   */
   // geometry_msgs::Vector3 rpydata; // Roll pitch yaw data in NWU format
-  tf::Quaternion q = tf::createQuaternionFromRPY(data.rpydata.x, data.rpydata.y,
-                                                 data.rpydata.z);
+  tf::Quaternion q = tf::createQuaternionFromRPY(
+      data_.rpydata.x, data_.rpydata.y, data_.rpydata.z);
 
   // geometry_msgs::Vector3 localpos; // Local pos based on home NWU format
   qrotor_backstepping_state.pose(
-      q, tf::Vector3(data.localpos.x, data.localpos.y, data.localpos.z));
+      q, tf::Vector3(data_.localpos.x, data_.localpos.y, data_.localpos.z));
 
   // geometry_msgs::Vector3 linvel;   // Linear velocity of quadcopter
-  qrotor_backstepping_state.v(data.linvel.x, data.linvel.y, data.linvel.z);
+  qrotor_backstepping_state.v(data_.linvel.x, data_.linvel.y, data_.linvel.z);
 
   // geometry_msgs::Vector3 omega;   // Angular velocities in NWU format
-  qrotor_backstepping_state.w(data.omega.x, data.omega.y, data.omega.z);
+  qrotor_backstepping_state.w(data_.omega.x, data_.omega.y, data_.omega.z);
 
   /* Objective
   * Acquire thrust, thrust_dot
   */
   // geometry_msgs::Vector3 linacc;   // Linear acceleration of quadcopter
   // data.linacc.z;
-  qrotor_backstepping_state.thrust;
+  qrotor_backstepping_state.thrust = thrust_;
 
-  qrotor_backstepping_state.thrust_dot;
+  qrotor_backstepping_state.thrust_dot = thrust_dot_;
 
   sensor_data = std::make_pair(current_time, qrotor_backstepping_state);
+
+  thrust_gain_estimator_.addSensorData(data_.rpydata.x, data_.rpydata.y,
+                                       data_.linacc.z);
 
   return true;
 }
 
 void QrotorBacksteppingControllerConnector::sendControllerCommands(
-    QrotorBacksteppingControl control);
+    QrotorBacksteppingControl control,
+    std::pair<double, QrotorBacksteppingState> &sensor_data);
 {
-  double Torque = control.torque;
-  tf::Vector3 Thrust_ddot = control.thrust_ddot;
+  double Thrust_ddot = control.thrust_ddot;
+  tf::Vector3 Torque = control.torque;
   /* Objective
   *Use torque, thrust_ddot -> get rpyratethrust
   */
+  thrust_dot_ = Thrust_ddot * dt_;
+  thrust_ = thrust_dot_ * dt_;
+
+  QrotorBacksteppingState current_state = std::get<1> sensor_data;
+  // conversions
+  Eigen::Vector3d torque;
+  tf::vectorTFToEigen(Torque, torque);
+  Eigen::Vector3d current_omega;
+  tf::vectorTFToEigen(current_state.omega, current_omega);
+  Eigen::Vector3d current_rpy;
+  current_rpy << data_rpydata.x, data_rpydata.y, data_rpydata.z;
+
+  Eigen::Vector3d omega_dot_cmd =
+      J_.llt().solve(J_ * current_omega.cross(current_omega) + torque);
+  Eigen::Vector3 omega_cmd = omega_dot_cmd * dt_;
+
+  Eigen::Vector3d rpydot_cmd = omegaToRpyDot(omega_cmd, current_rpy);
+
   geometry_msgs::Quaternion rpyt_msg;
-  // rpyt_msg.x; // Roll
-  // rpyt_msg.y; // Pitch
-  // rpyt_msg.z; // Yaw rate
-  // rpyt_msg.w; // thrust
+  rpyt_msg.x = rpydot_cmd[0] * dt_; // Roll command
+  rpyt_msg.y = rpydot_cmd[1] * dt_; // Pitch command
+  rpyt_msg.z = rpydot_cmd[2];       // Yaw rate command
+  rpyt_msg.w =
+      thrust_ / (m_ * thrust_gain_estimator_.getThrustGain()); // thrust_command
   drone_hardware_.cmdrpyawratethrust(rpyt_msg);
+}
+
+void QrotorBacksteppingControllerConnector::setGoal(
+    std::shared_ptr<ReferenceTrajectory<ParticleState, Snap>> goal) {
+  BaseClass::setGoal(goal);
+  t_0_ = std::chrono::high_resolution_clock::now();
+  thrust_ = m_ * g_;
+  thrust_dot_ = 0.0;
+  // setThrust(config_.mass() * config_.acc_gravity());
+  // setThrustDot(0.0);
+  VLOG(1) << "Clearing thrust estimator buffer";
+  thrust_gain_estimator_.clearBuffer();
+}
+
+Eigen::Vector3d QrotorBacksteppingControllerConnector::omegaToRpyDot(
+    const Eigen::Vector3d &omega, const Eigen::Vector3d &rpy) {
+  Eigen::Matrix3d Mrpy;
+  double s_roll = sin(rpy[0]), c_roll = cos(rpy[0]), t_pitch = tan(rpy[1]);
+  double sec_pitch = sqrt(1 + t_pitch * t_pitch);
+  Mrpy << 1, s_roll * t_pitch, c_roll * t_pitch, 0, c_roll, -s_roll, 0,
+      s_roll * sec_pitch, c_roll * sec_pitch;
+  return Mrpy * omega;
 }
