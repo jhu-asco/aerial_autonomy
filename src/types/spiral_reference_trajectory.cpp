45da151f430595955c2b5110cc41bb0d014040fe
yorgoon <soowk311@gmail.com>
2018-08-15 15:18:00 -0400

    atGoalEnd returns atTime(0)

--- src/types/spiral_reference_trajectory.cpp
+++ src/types/spiral_reference_trajectory.cpp
@@ -128,7 +128,5 @@ SpiralReferenceTrajectory::atTime(double t) const {
 
 std::pair<Eigen::VectorXd, Eigen::VectorXd>
 SpiralReferenceTrajectory::atGoalEnd() const {
-  Eigen::VectorXd x(21);
-  Eigen::VectorXd u(6);
-  return std::make_pair(x, u);
+  return atTime(0);
 }
