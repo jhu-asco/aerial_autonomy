e004cb250ddac481bb43bcb8714d28d363cf7359
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2018-08-28 23:26:49 -0400

    Add noise at end of polynomial reference

--- tests/types/polynomial_reference_trajectory_tests.cpp
+++ tests/types/polynomial_reference_trajectory_tests.cpp
@@ -15,22 +15,21 @@ TEST(PolynomialReferenceTrajectory, Initialize) {
 // Test Basis
 TEST(PolynomialReferenceTrajectory, CheckBasis) {
   PolynomialReferenceConfig config;
-  config.set_tf(2);
   PositionYaw start_position_yaw(0, 0, 0, 0);
   PositionYaw goal_position_yaw(0, 0, 0, 0);
   PolynomialReferenceTrajectory reference(goal_position_yaw, start_position_yaw,
                                           config);
-  Eigen::MatrixXd basis = reference.findBasisMatrix(config.tf(), 9, 4);
+  Eigen::MatrixXd basis = reference.findBasisMatrix(config.min_tf(), 9, 4);
   ASSERT_EQ(basis.rows(), 5);
   ASSERT_EQ(basis.cols(), 10);
   // sixth column
-  ASSERT_DOUBLE_EQ(basis(0, 5), std::pow(config.tf(), 5));
-  ASSERT_DOUBLE_EQ(basis(1, 5), 5 * std::pow(config.tf(), 4));
-  ASSERT_DOUBLE_EQ(basis(2, 5), 5 * 4 * std::pow(config.tf(), 3));
-  ASSERT_DOUBLE_EQ(basis(3, 5), 5 * 4 * 3 * std::pow(config.tf(), 2));
-  ASSERT_DOUBLE_EQ(basis(4, 5), 5 * 4 * 3 * 2 * std::pow(config.tf(), 1));
+  ASSERT_DOUBLE_EQ(basis(0, 5), std::pow(config.min_tf(), 5));
+  ASSERT_DOUBLE_EQ(basis(1, 5), 5 * std::pow(config.min_tf(), 4));
+  ASSERT_DOUBLE_EQ(basis(2, 5), 5 * 4 * std::pow(config.min_tf(), 3));
+  ASSERT_DOUBLE_EQ(basis(3, 5), 5 * 4 * 3 * std::pow(config.min_tf(), 2));
+  ASSERT_DOUBLE_EQ(basis(4, 5), 5 * 4 * 3 * 2 * std::pow(config.min_tf(), 1));
   // second column
-  ASSERT_DOUBLE_EQ(basis(0, 1), config.tf());
+  ASSERT_DOUBLE_EQ(basis(0, 1), config.min_tf());
   ASSERT_DOUBLE_EQ(basis(1, 1), 1);
   ASSERT_DOUBLE_EQ(basis(2, 1), 0);
   ASSERT_DOUBLE_EQ(basis(3, 1), 0);
@@ -39,11 +38,11 @@ TEST(PolynomialReferenceTrajectory, CheckBasis) {
 // Test trajectory and velocities etc
 TEST(PolynomialReferenceTrajectory, CheckTrajectory) {
   PolynomialReferenceConfig config;
-  config.set_tf(5);
   PositionYaw start_position_yaw(0, 0, 0, 1);
   PositionYaw goal_position_yaw(1, 2, 3, 1.5);
   PolynomialReferenceTrajectory reference(goal_position_yaw, start_position_yaw,
                                           config);
+  double tf = Eigen::Vector3d(1, 2, 3).norm() / config.max_velocity();
   auto start = reference.atTime(0);
   ASSERT_NEAR(start.first[0], start_position_yaw.x, 1e-7);
   ASSERT_NEAR(start.first[1], start_position_yaw.y, 1e-7);
@@ -66,7 +65,7 @@ TEST(PolynomialReferenceTrajectory, CheckTrajectory) {
   ASSERT_DOUBLE_EQ(start.second[2], 0.0);
   ASSERT_DOUBLE_EQ(start.second[3], 0.0);
   // End
-  auto end = reference.atTime(5);
+  auto end = reference.atTime(tf);
   ASSERT_NEAR(end.first[0], goal_position_yaw.x, 1e-7);
   ASSERT_NEAR(end.first[1], goal_position_yaw.y, 1e-7);
   ASSERT_NEAR(end.first[2], goal_position_yaw.z, 1e-7);
@@ -88,10 +87,51 @@ TEST(PolynomialReferenceTrajectory, CheckTrajectory) {
   ASSERT_NEAR(end.second[2], 0.0, 1e-7);
   ASSERT_NEAR(end.second[3], 0.0, 1e-7);
 }
+TEST(PolynomialReferenceTrajectory, Noise) {
+  PolynomialReferenceConfig config;
+  PositionYaw start_position_yaw(0, 0, 0, -2);
+  PositionYaw goal_position_yaw(1, 2, 3, 1.5);
+  PolynomialReferenceTrajectory reference(goal_position_yaw, start_position_yaw,
+                                          config);
+  double t = 1.0, a = 0.5, nu = 0.1, dt = 1e-6;
+  Eigen::Vector3d noise = reference.getNoise(t, a, nu);
+  Eigen::Vector3d noise_dt = reference.getNoise(t + dt, a, nu);
+  Eigen::Vector3d noise_neg_dt = reference.getNoise(t - dt, a, nu);
+  ASSERT_DOUBLE_EQ(noise[0], a * std::pow(sin(2 * M_PI * nu * t), 2));
+  ASSERT_NEAR(noise[1], (noise_dt[0] - noise_neg_dt[0]) / (2 * dt), 1e-6);
+  ASSERT_NEAR(noise[2], (noise_dt[1] - noise_neg_dt[1]) / (2 * dt), 1e-6);
+}
+
+TEST(PolynomialReferenceTrajectory, CheckTrajectoryWithNoise) {
+  PolynomialReferenceConfig config;
+  config.set_add_noise(true);
+  PositionYaw start_position_yaw(0, 0, 0, 1);
+  PositionYaw goal_position_yaw(1, 2, 3, M_PI / 2.0);
+  PolynomialReferenceTrajectory reference(goal_position_yaw, start_position_yaw,
+                                          config);
+  double tf = Eigen::Vector3d(1, 2, 3).norm() / config.max_velocity();
+  double delta_t = 0.5;
+  Eigen::VectorXd state1 = reference.atTime(tf + delta_t).first;
+  Eigen::VectorXd state2 = reference.atTime(tf + delta_t + 1e-6).first;
+  double omega = 2 * M_PI * config.forward_noise_frequency();
+  double omega_z = 2 * M_PI * config.z_noise_frequency();
+  double noise =
+      config.forward_noise_amplitude() * std::pow(sin(omega * delta_t), 2);
+  double noise_z =
+      config.z_noise_amplitude() * std::pow(sin(omega_z * delta_t), 2);
+  ASSERT_NEAR(state1[1], 2 + noise, 1e-8);
+  ASSERT_NEAR(state1[0], 1, 1e-8);
+  ASSERT_NEAR(state1[2], 3 + noise_z, 1e-8);
+  // Velocity
+  Eigen::Vector3d vel = (state2.head(3) - state1.head(3)) / 1e-6;
+  ASSERT_NEAR(state1[6], vel[0], 1e-6);
+  ASSERT_NEAR(state1[7], vel[1], 1e-6);
+  ASSERT_NEAR(state1[8], vel[2], 1e-6);
+}
 
 TEST(PolynomialReferenceTrajectory, PrintTrajectory) {
   PolynomialReferenceConfig config;
-  config.set_tf(5);
+  config.set_add_noise(true);
   PositionYaw start_position_yaw(0, 0, 0, -2);
   PositionYaw goal_position_yaw(1, 2, 3, 1.5);
   PolynomialReferenceTrajectory reference(goal_position_yaw, start_position_yaw,
