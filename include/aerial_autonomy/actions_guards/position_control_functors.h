c233e584a742726b30cd22109d45744d40f86b7b
yorgoon <soowk311@gmail.com>
2018-09-19 17:04:51 -0400

    Replace RPYT with Backstepping in ControlTransitionActionFunctor. Template connector in ControlInternalActionFunctor.

--- include/aerial_autonomy/actions_guards/position_control_functors.h
+++ include/aerial_autonomy/actions_guards/position_control_functors.h
@@ -2,26 +2,53 @@
 #include <aerial_autonomy/actions_guards/base_functors.h>
 #include <aerial_autonomy/actions_guards/hovering_functors.h>
 #include <aerial_autonomy/actions_guards/shorting_action_sequence.h>
+#include <aerial_autonomy/common/conversions.h>
 #include <aerial_autonomy/controller_connectors/position_controller_drone_connector.h>
+#include <aerial_autonomy/controller_connectors/qrotor_backstepping_controller_connector.h>
 #include <aerial_autonomy/logic_states/base_state.h>
 #include <aerial_autonomy/robot_systems/uav_system.h>
+#include <aerial_autonomy/types/minimum_snap_reference_trajectory.h>
 #include <aerial_autonomy/uav_basic_events.h>
 #include <glog/logging.h>
 #include <parsernode/common.h>
 
 namespace be = uav_basic_events;
 
-/**
-* @brief Transition action to perform when going into position control mode
-*
-* @tparam LogicStateMachineT Logic state machine used to process events
-*/
 template <class LogicStateMachineT>
 struct PositionControlTransitionActionFunctor_
     : ActionFunctor<PositionYaw, UAVSystem, LogicStateMachineT> {
   void run(const PositionYaw &goal, UAVSystem &robot_system) {
+    tf::StampedTransform start_pose = robot_system.getPose();
+
+    PositionYaw start_position_yaw;
+    conversions::tfToPositionYaw(start_position_yaw, start_pose);
+    // Minimum snap reference trajectory config
+    auto reference_config =
+        this->state_machine_config_.minimum_snap_reference_trajectory_config();
+    // Waypoints config
+    auto waypoint_config =
+        reference_config.mutable_following_waypoint_sequence_config();
+    // Add waypoints
+    conversions::setWaypoint(waypoint_config->add_way_points(),
+                             start_position_yaw);
+    conversions::setWaypoint(waypoint_config->add_way_points(), goal);
+    // L2 distance between start, goal
+    double x = start_position_yaw.x - goal.x;
+    double y = start_position_yaw.x - goal.y;
+    double z = start_position_yaw.x - goal.z;
+    double distance = std::sqrt(x * x + y * y + z * z);
+
+    double average_velocity = reference_config.average_velocity();
+    // Add time interval
+    reference_config.add_tau_vec(distance / average_velocity);
+    // Minimum snap reference trajectory object
+    std::shared_ptr<ReferenceTrajectory<ParticleState, Snap>> reference(
+        new MinimumSnapReferenceTrajectory(reference_config));
+
     robot_system
-        .setGoal<RPYTBasedPositionControllerDroneConnector, PositionYaw>(goal);
+        .setGoal<QrotorBacksteppingControllerConnector,
+                 std::shared_ptr<ReferenceTrajectory<ParticleState, Snap>>>(
+            reference);
   }
 };
 
@@ -92,8 +119,13 @@ template <class LogicStateMachineT>
 struct PositionControlTransitionGuardFunctor_
     : GuardFunctor<PositionYaw, UAVSystem, LogicStateMachineT> {
   bool guard(const PositionYaw &goal, UAVSystem &robot_system) {
-    parsernode::common::quaddata data = robot_system.getUAVData();
-    geometry_msgs::Vector3 current_position = data.localpos;
+    // parsernode::common::quaddata data = robot_system.getUAVData();
+    // geometry_msgs::Vector3 current_position = data.localpos;
+
+    geometry_msgs::Vector3 current_position;
+    tf::StampedTransform current_pose = robot_system.getPose();
+    tf::vector3TFToMsg(current_pose.getOrigin(), current_position);
+
     // Check goal is close to position before sending goal (Can use a geofence
     // here)
     const double &tolerance_pos =
@@ -135,14 +167,15 @@ struct ZeroVelocityGuardFunctor_
 /**
  * @brief internal action while performing position control
  *
-* @tparam LogicStateMachineT Logic state machine used to process events
+ * @tparam LogicStateMachineT Logic state machine used to process events
+ * @tparam ControllerConnectorT Controller connector used to process events
  */
-template <class LogicStateMachineT>
+template <class LogicStateMachineT, class ControllerConnectorT>
 using PositionControlInternalActionFunctor_ =
-    boost::msm::front::ShortingActionSequence_<boost::mpl::vector<
-        UAVStatusInternalActionFunctor_<LogicStateMachineT>,
-        ControllerStatusInternalActionFunctor_<
-            LogicStateMachineT, RPYTBasedPositionControllerDroneConnector>>>;
+    boost::msm::front::ShortingActionSequence_<
+        boost::mpl::vector<UAVStatusInternalActionFunctor_<LogicStateMachineT>,
+                           ControllerStatusInternalActionFunctor_<
+                               LogicStateMachineT, ControllerConnectorT>>>;
 
 /**
 * @brief State that uses position control functor to reach a desired goal.
@@ -150,6 +183,18 @@ using PositionControlInternalActionFunctor_ =
 * @tparam LogicStateMachineT Logic state machine used to process events
 */
 template <class LogicStateMachineT>
-using ReachingGoal_ =
+using ReachingGoal_ = BaseState<
+    UAVSystem, LogicStateMachineT,
+    PositionControlInternalActionFunctor_<
+        LogicStateMachineT, RPYTBasedPositionControllerDroneConnector>>;
+
+/**
+* @brief State that uses backstepping control functor to reach a desired goal.
+*
+* @tparam LogicStateMachineT Logic state machine used to process events
+*/
+template <class LogicStateMachineT>
+using ReachingGoalBackStepping_ =
     BaseState<UAVSystem, LogicStateMachineT,
-              PositionControlInternalActionFunctor_<LogicStateMachineT>>;
+              PositionControlInternalActionFunctor_<
+                  LogicStateMachineT, QrotorBacksteppingControllerConnector>>;
