f959f641fda0a593df79be476b72f39e146e29f9
Cora Dimmig <cdimmig@jhu.edu>
2022-02-25 10:39:02 -0500

    Created a separate gripper pick and place state machine

---
+++ src/system_handler_nodes/uav_gripper_arm_system_node.cpp
@@ -0,0 +1,34 @@
+#include <aerial_autonomy/common/system_handler_node_utils.h>
+#include <aerial_autonomy/pick_place_events.h>
+#include <aerial_autonomy/state_machines/gripper_pick_place_state_machine.h>
+#include <aerial_autonomy/system_handlers/uav_arm_system_handler.h>
+
+/**
+ * @brief Loads configutation file and starts system
+ * @param argc Number of arguments
+ * @param argv Arguments
+ * @return Exit status
+ */
+int main(int argc, char **argv) {
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+  google::InitGoogleLogging("aerial_autonomy_node");
+
+  ros::init(argc, argv, "aerial_autonomy");
+  ros::NodeHandle nh;
+
+  createAndConfigureLogConfig(nh);
+  auto state_machine_config = loadConfigFromROSParam<BaseStateMachineConfig>(
+      nh, "state_machine_config_filename");
+  auto uav_system_handler_config =
+      loadConfigFromROSParam<UAVSystemHandlerConfig>(
+          nh, "uav_system_config_filename");
+
+  UAVArmSystemHandler<
+      GripperPickPlaceStateMachine,
+      pick_place_events::PickPlaceEventManager<GripperPickPlaceStateMachine>>
+      uav_system_handler(uav_system_handler_config, state_machine_config);
+
+  ros::spin();
+
+  return 0;
+}
